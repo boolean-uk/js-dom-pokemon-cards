140e6d37401c8511d38ac37c6e2d4b2290ad8638
Marin Balabanov <marincomics@gmail.com>
2022-08-30 13:08:17 +0200

    Adds first working version with some styling issues

--- src/index.js
+++ src/index.js
@@ -4,3 +4,32 @@ console.log(data);
 //You can start simple and just render a single 
 //pokemon card from the first element
 console.log(data[0]);
+
+let pokeList = document.querySelector('ul')
+
+let pokeCards = []
+
+for (let pokeMon of data) {
+    const thisPokeMonCard = `
+<li class="card">
+  <h2 class="card--title">${pokeMon.name}</h2>
+  <img
+    width="256"
+    class="card--img"
+    src="${pokeMon.sprites.other['official-artwork'].front_default}"
+  />
+  <ul class="card--text">
+    <li>HP: ${pokeMon.stats[0].base_stat}</li>
+    <li>ATTACK: ${pokeMon.stats[1].base_stat}</li>
+    <li>DEFENSE: ${pokeMon.stats[2].base_stat}</li>
+    <li>SPECIAL-ATTACK: ${pokeMon.stats[3].base_stat}</li>
+    <li>SPECIAL-DEFENSE: ${pokeMon.stats[4].base_stat}</li>
+    <li>SPEED: ${pokeMon.stats[5].base_stat}</li>
+  </ul>
+</li>
+    `
+
+pokeCards.push(thisPokeMonCard)
+}
+
+pokeList.innerHTML = pokeCards
