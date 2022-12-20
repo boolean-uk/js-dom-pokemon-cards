1828eb6911e3877715f2ecbdff8d923ed3094074
Kat <katdemey@outlook.com>
2022-12-20 12:56:24 +0000

    step 1 and two

--- src/index.js
+++ src/index.js
@@ -4,3 +4,26 @@ console.log(data);
 //You can start simple and just render a single 
 //pokemon card from the first element
 console.log(data[0]);
+
+const pokemonUL = document.querySelector(".cards")
+
+//about 50lines of code expected- 
+
+for(let i=0; i < data.length; i++) {
+    const pokemonObj = data[i]
+    
+    const pokemonLi = document.createElement('li')
+    pokemonLi.setAttribute('class', 'card')
+    pokemonUL.append(pokemonLi)
+    
+    const pokemonName = document.createElement('h2')
+    pokemonName.setAttribute('class', 'card--title')
+    pokemonName.innerText = pokemonObj.name
+    pokemonLi.append(pokemonName)
+
+    
+
+    //create the card HTML elements
+    //append/stitch them together
+    //add the li to the ul
+}
