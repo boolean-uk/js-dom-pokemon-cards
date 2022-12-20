7a735fbfde747ebc294fbbd265d389afc2ec6703
MaybeFreak <michaelpope97@gmail.com>
2022-12-20 15:07:03 +0100

    core done

--- src/index.js
+++ src/index.js
@@ -1,6 +1,59 @@
 
 console.log(data);
 
-//You can start simple and just render a single 
-//pokemon card from the first element
+const target = document.querySelector('ul.cards')
+
+const getName = (pokemon) => {
+    return capatilizeName(pokemon.name)
+}
+
+const capatilizeName = (str) => {
+    return str.charAt(0).toUpperCase() + str.slice(1).toLowerCase()
+}
+
+const createName = (pokemon) => {
+    const element = document.createElement('h2')
+    element.innerText = getName(pokemon)
+    return element
+}
+
+const getImg = (pokemon) => {
+    return pokemon.sprites.other['official-artwork'].front_default
+}
+
+const createImg = (pokemon) => {
+    const element = document.createElement('img')
+    element.setAttribute('width', '256')
+    element.setAttribute('class', 'card--img')
+    element.setAttribute('src', getImg(pokemon))
+    return element
+}
+
+const createStats = (pokemon) => {
+    const ul = document.createElement('ul')
+    ul.setAttribute('class', 'card--text')
+    pokemon.stats.forEach(element => {
+        const li = document.createElement('li')
+        li.innerText = `${element.stat.name.toUpperCase()}: ${element.base_stat}`
+        ul.append(li)
+    })
+    return ul
+}
+
+const populateList = (data) => {
+    data.forEach(pokemon => {
+        const card = document.createElement('li')
+        card.setAttribute('class', 'card')
+        const title = createName(pokemon)
+        card.append(title)
+        const img = createImg(pokemon)
+        card.append(img)
+        const ul = createStats(pokemon)
+        card.append(ul)
+        target.append(card)
+    })
+}
+
+populateList(data)
+
 console.log(data[0]);
