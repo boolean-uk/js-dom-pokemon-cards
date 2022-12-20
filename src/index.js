54863853e56fa8fd71c23832bc1ff4c0c367c2dc
Chris Sach <christophersach90@gmail.com>
2022-12-20 15:18:34 +0000

    completed core requirements

--- src/index.js
+++ src/index.js
@@ -13,15 +13,51 @@ for(let i=0; i < data.length; i++) {
     const pokemonObj = data[i]
     
     const pokemonLi = document.createElement('li')
+    pokemonLi.style.listStyle = `none`
     pokemonLi.setAttribute('class', 'card')
     pokemonUL.append(pokemonLi)
     
     const pokemonName = document.createElement('h2')
     pokemonName.setAttribute('class', 'card--title')
-    pokemonName.innerText = pokemonObj.name
-    pokemonLi.append(pokemonName)
+    let pName = pokemonObj.name
+    pName = pName[0].toUpperCase() + pName.slice(1)
+    pokemonName.innerText = pName
+    pokemonLi.append(pokemonName)    
 
-    
+    const pokeImage = new Image(256);
+    pokeImage.src = pokemonObj.sprites.other['official-artwork'].front_default
+    pokeImage.setAttribute('class', 'card--img')
+    pokemonLi.append(pokeImage)
+
+    const pokeStats = document.createElement('ul');
+    pokeStats.style.listStyle = `none`
+    pokeStats.setAttribute('class', 'card--text');
+    pokemonLi.append(pokeStats);
+
+    const pokeHP = document.createElement('li');
+    pokeHP.innerText = "HP:  " + pokemonObj.stats[0].base_stat;
+    pokeStats.append(pokeHP);
+
+    const pokeAtt = document.createElement('li');
+    pokeAtt.innerText = "ATTACK:  " + pokemonObj.stats[1].base_stat;
+    pokeStats.append(pokeAtt);
+
+    const pokeDef = document.createElement('li');
+    pokeDef.innerText = "DEFENCE:  " + pokemonObj.stats[2].base_stat;
+    pokeStats.append(pokeDef);
+
+    const pokeSpecAtt = document.createElement('li');
+    pokeSpecAtt.innerText = "SPECIAL-ATTACK:  " + pokemonObj.stats[3].base_stat;
+    pokeStats.append(pokeSpecAtt);
+
+    const pokeSpecDef = document.createElement('li');
+    pokeSpecDef.innerText = "SPECIAL-DEFENCE:  " + pokemonObj.stats[4].base_stat;
+    pokeStats.append(pokeSpecDef);
+
+    const pokeSpeed = document.createElement('li');
+    pokeSpeed.innerText = "SPEED:  " + pokemonObj.stats[5].base_stat;
+    pokeStats.append(pokeSpeed);
+
 
     //create the card HTML elements
     //append/stitch them together
