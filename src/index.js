412a573042574e9b0ea16302d31ba8904f228e3f
Todd Griffin <todd.griffin61@outlook.com>
2023-04-12 15:55:50 +0100

    started extension

--- src/index.js
+++ src/index.js
@@ -17,23 +17,49 @@ const masterUl = document.querySelector("body > ul")
 // masterUl.append(cardContent2)
 // masterUl.append(cardContent)
 
+masterUl.style.listStyle = 'none'
+
 for (let i = 0; i < data.length; i++) {
-    const cardContent = document.createElement('li')
-    cardContent.classList.add('card', `${i}`)
-    masterUl.appendChild(cardContent)
-    
-    const cardTitle = document.createElement('h2')
-    cardTitle.classList.add('card--title')
-    cardTitle.innerText = data[i]['name']
-    cardContent.appendChild(cardTitle)
+  const cardContent = document.createElement('li')
+  cardContent.classList.add('card', `${i}`)
+  masterUl.appendChild(cardContent)
+  
+  const cardTitle = document.createElement('h2')
+  cardTitle.classList.add('card--title')
+  cardTitle.innerText = data[i]['name']
+  cardContent.appendChild(cardTitle)
 
-    const cardImg = document.createElement('img')
-    cardImg.classList.add('card--img')
-    cardImg.src = data[i]['sprites']['front_default']
-    cardContent.appendChild(cardImg)
-    cardImg.width = 256;
+  const cardImg = document.createElement('img')
+  cardImg.classList.add('card--img')
+  cardImg.src = data[i]['sprites']['other']['official-artwork']['front_default']
+  cardContent.appendChild(cardImg)
+  cardImg.width = 256;
 
+  const attrList = document.createElement('ul')
+  attrList.classList.add('card--text')
+
+  for (let j = 0; j < 6; j++) {
+    const attribute = document.createElement('li')
+    const statName = data[i]['stats'][j]['stat']['name']
+    const statValue = data[i]['stats'][j]['base_stat']
+    const statItem = `${statName.toUpperCase()}: ${statValue}`
+    attribute.innerText = statItem
+    attrList.appendChild(attribute)
+
+  }
+  attrList.style.listStyle = 'none'
+  cardContent.appendChild(attrList)
+
+  const versionList = document.createElement('ul')
+  versionList.classList.add('card--text')
+
+  for (let j = 0; j < data[i]['game_indices'].length; j++) {
+    const versionNameItem = document.createElement('li')
+    const versionName = data[i]['game_indices'][j]['version']['name']
+  }
 }
+
+
 
 
 
