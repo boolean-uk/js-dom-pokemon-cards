2c983d25d8c8e7f04f1b003ee3ba1cb4b6b5c2cc
Lee Kelly <leeowenkelly@gmail.com>
2023-04-12 15:20:39 +0100

    title and images added

--- src/index.js
+++ src/index.js
@@ -10,17 +10,31 @@ console.log(data[0]);
 
 
 const masterUl = document.querySelector("body > ul")
-const cardContent = document.createElement('li')
+
 // const cardContent2 = document.createElement('li')
 //
 
-
-masterUl.append(cardContent)
 // masterUl.append(cardContent2)
 // masterUl.append(cardContent)
 
-for (let i = 0; i <= data.length; i++) {
-  cardContent.classList.add('card', `${i}`)
+for (let i = 0; i < data.length; i++) {
+    const cardContent = document.createElement('li')
+    cardContent.classList.add('card', `${i}`)
+    masterUl.appendChild(cardContent)
+    
+    const cardTitle = document.createElement('h2')
+    cardTitle.classList.add('card--title')
+    cardTitle.innerText = data[i]['name']
+    cardContent.appendChild(cardTitle)
+
+    const cardImg = document.createElement('img')
+    cardImg.classList.add('card--img')
+    cardImg.src = data[i]['sprites']['front_default']
+    cardContent.appendChild(cardImg)
+    cardImg.width = 256;
+
 }
+
+
 
 console.log(data.length)
