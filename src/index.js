c5da0b1be34ace817dfa19e5da0dcefe11e0a49b
Alex Shaw <alexjshaw@gmail.com>
2023-04-12 17:26:06 +0100

    couple of tweaks to layout

--- src/index.js
+++ src/index.js
@@ -5,30 +5,27 @@ console.log(data);
 //pokemon card from the first element
 console.log(data[0]);
 
-// Create a constant for each element we have to insert e.g. h2, img, ul
-// for loop for number of cards i.e. length of data array
-// append elements in order
-
 const pokemonList = document.querySelector(".cards")
 
-
 for (let i = 0; i < data.length; i++) {
-
     const currentObject = data[i]
 
 // Card Body
+
     const pokemonCard = document.createElement("li");
     pokemonCard.style.listStyleType = "none"
     pokemonCard.setAttribute('class', 'card');
     pokemonList.append(pokemonCard);
 
 // Card Title
+
     const pokemonTitle = document.createElement('h2');
     pokemonTitle.setAttribute('class', 'card--title');
     pokemonTitle.innerText = currentObject.name.charAt(0).toUpperCase() + currentObject.name.slice(1);
     pokemonCard.append(pokemonTitle);
 
 // Card Image
+
     const pokemonImage = document.createElement('img')
     pokemonImage.setAttribute('width', '256');
     pokemonImage.setAttribute('class', 'card--img');
@@ -43,8 +40,8 @@ for (let i = 0; i < data.length; i++) {
       pokemonImage.addEventListener('click', toggleImage);
     pokemonCard.append(pokemonImage);
     
-
 // Card Stats
+
     const pokemonCardTextList = document.createElement("ul");
     pokemonCardTextList.setAttribute('class', 'card--text');
     pokemonCard.append(pokemonCardTextList);
