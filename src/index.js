2c864dbd2325000e275d9bc5a50f70d3f1ac8a6e
Koci H <husnukoci94@gmail.com>
2023-04-12 19:59:35 +0300

    core is done

--- src/index.js
+++ src/index.js
@@ -26,13 +26,41 @@ for (let i = 0; i <= 19; i++) {
   const img = document.createElement("img");
   img.src = data[i].sprites.other["official-artwork"].front_default;
   li.append(img);
+  img.setAttribute("class", "card--img")
+  img.setAttribute("width", "256")
 
   const newUl = document.createElement('ul');
   newUl.setAttribute("class", "card--text");
   li.append(newUl)
+
   const newLi = document.createElement('li');
   newLi.innerText = (data[i].stats[0].stat.name).toLocaleUpperCase() + ": " + data[i].stats[0].base_stat
   newUl.append(newLi)
+
+  const newLiTwo = document.createElement('li');
+  newLiTwo.innerText = (data[i].stats[1].stat.name).toLocaleUpperCase() + ": " + data[i].stats[1].base_stat
+  newUl.append(newLiTwo)
+
+  const newLiThree = document.createElement('li');
+  newLiThree.innerText = (data[i].stats[2].stat.name).toLocaleUpperCase() + ": " + data[i].stats[2].base_stat
+  newUl.append(newLiThree)
+
+  const newLiFour = document.createElement('li');
+  newLiFour.innerText = (data[i].stats[3].stat.name).toLocaleUpperCase() + ": " + data[i].stats[3].base_stat
+  newUl.append(newLiFour)
+
+  const newLiFive = document.createElement('li');
+  newLiFive.innerText = (data[i].stats[4].stat.name).toLocaleUpperCase() + ": " + data[i].stats[4].base_stat
+  newUl.append(newLiFive)
+
+  const newLiSix = document.createElement('li');
+  newLiSix.innerText = (data[i].stats[5].stat.name).toLocaleUpperCase() + ": " + data[i].stats[5].base_stat
+  newUl.append(newLiSix)
+
+  newUl.style.listStyleType = 'none';
+
+
+
 
 }
 console.log(data);
