fe3466994977da6524bb86d3cde1f5a518d0b210
marc-alzapiedi <marcalzapiedi@gmail.com>
2023-04-12 16:05:32 +0200

    some work

--- src/index.js
+++ src/index.js
@@ -4,3 +4,24 @@ console.log(data);
 //You can start simple and just render a single 
 //pokemon card from the first element
 console.log(data[0]);
+
+const cardUL = document.querySelector('ul')
+
+for (let i = 0; i < data.length; i++){
+   const entry = data[i]
+   console.log(entry)
+
+   // create cards:
+   const cardTile = document.createElement('li')
+   cardTile.setAttribute("class", "card")
+   cardUL.append(cardTile)
+
+
+   // Card title
+   const pokeName = document.createElement('h2')
+   pokeName.innerText = entry.name
+   pokeName.setAttribute("class", "card--title")
+   cardTile.append(pokeName)
+
+
+}
