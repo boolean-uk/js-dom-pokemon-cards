025d168611ff3d2760f930eeccd068f16584b13a
Todd Griffin <todd.griffin61@outlook.com>
2023-04-12 13:39:32 +0100

    experimenting with ways to add many li to one ul

--- src/index.js
+++ src/index.js
@@ -1,6 +1,26 @@
 
+
+
 console.log(data);
 
 //You can start simple and just render a single 
 //pokemon card from the first element
 console.log(data[0]);
+
+
+
+const masterUl = document.querySelector("body > ul")
+const cardContent = document.createElement('li')
+// const cardContent2 = document.createElement('li')
+//
+
+
+masterUl.append(cardContent)
+// masterUl.append(cardContent2)
+// masterUl.append(cardContent)
+
+for (let i = 0; i <= data.length; i++) {
+  cardContent.classList.add('card', `${i}`)
+}
+
+console.log(data.length)
