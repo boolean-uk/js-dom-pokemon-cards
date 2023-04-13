20738573aec8d4906e325f11d2168a67b32fae92
PanagiotisTziolos <panagiotistziolos@outlook.com>
2023-04-13 15:24:26 +0300

    completed main part

--- src/index.js
+++ src/index.js
@@ -13,18 +13,15 @@ for (let i = 0; i < data.length; i++) {
   const card = document.createElement("li");
   // add the class card to li
   card.classList.add("card");
+  // remove the dot of the li
+  cards.style.listStyleType = "none";
 
   // create a h2 with class card--title and add it to li
   const card__title = document.createElement("h2");
   card__title.setAttribute("class", "card--title");
   // set h2 to the name of the pokemon
-  card__title.innerText = data[i].name;
-  // append to li
-  card.append(card__title);
-  // append card to the list of cards
-  cards.append(card);
-  // remove the dot of the li
-  cards.style.listStyleType = "none";
+  card__title.innerText = data[i].name[0].toUpperCase() + data[i].name.substring(1);
+  
 
   // create an img and add the source and class card--img to it
   const card__img = document.createElement("img");
@@ -33,11 +30,35 @@ for (let i = 0; i < data.length; i++) {
     "src",
     data[i].sprites.other["official-artwork"].front_default
   );
+  card__img.setAttribute('width', 256)
+
+  // create a ul with class card--text
+  const card__text = document.createElement('ul')
+  card__text.setAttribute('class', 'card--text')
+  // remove the dot of the li
+  card__text.style.listStyleType = "none";
+  
+  // create a loop to add every li item to the ul
+  
+  for (let j = 0; j < data[i].stats.length; j++) {
+    // create an li element
+    const stat = document.createElement('li')
+    stat.style.padding = '5px'
+    const stat_name = data[i].stats[j].stat.name.toUpperCase()
+    const stat_value = data[i].stats[j].base_stat
+    stat.innerText = stat_name + ": " + stat_value
+
+    card__text.append(stat)
+  }
+
+
   // append img to card
+  card.append(card__title);
   card.append(card__img);
+  card.append(card__text);
+  
+  // append card to the list of cards
+  cards.append(card);
 }
 
-// create an img and add the source and class card--img to it
-
-// create a ul with class card--text
 // create a loop to add every li item to the ul
