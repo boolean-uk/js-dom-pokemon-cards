5bb3267cc00e4d439ccf583f509b69b5228f51e3
Aidan van Geest <aidanvangeest@gmail.com>
2023-04-13 13:01:45 +0200

    Created card

--- src/index.js
+++ src/index.js
@@ -1,6 +1,12 @@
 
-console.log(data);
+// console.log(data);
 
 //You can start simple and just render a single 
 //pokemon card from the first element
 console.log(data[0]);
+
+const cards = document.querySelector('.cards')
+const newCard = document.createElement('li')
+newCard.classList.add('card')
+newCard.innerText = "Bulbasaur"
+cards.append(newCard)
