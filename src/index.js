a70f3680c30f49baae3dcba20413ede705e6dadd
Kitti <kitti.horvath6891@gmail.com>
2023-04-13 13:24:15 +0200

    Added card: list and title

--- src/index.js
+++ src/index.js
@@ -4,3 +4,14 @@ console.log(data);
 //You can start simple and just render a single 
 //pokemon card from the first element
 console.log(data[0]);
+const cardList = document.querySelector('.cards')
+for(let i = 0; i < data.length; i++) {
+    const card = document.createElement('li')
+    card.classList.add('card')
+    const title = document.createElement('h2')
+    title.classList.add('card--title')
+    title.innerText=data[i].name
+    card.append(title)
+
+    cardList.append(card)
+}
