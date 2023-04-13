7c1e0f69deb0f74cbc3d9d80c97a2546fd0264fd
Isabelle <isabel.tartarelli@live.it>
2023-04-13 18:01:54 +0100

    Created cards

--- src/index.js
+++ src/index.js
@@ -1,6 +1,19 @@
+// console.log(data);
 
-console.log(data);
-
-//You can start simple and just render a single 
+//You can start simple and just render a single
 //pokemon card from the first element
-console.log(data[0]);
+// console.log(data[0]);
+
+
+const pokCard = document.querySelector(".cards");
+
+data.forEach((element) => {
+  // console.log(element)
+  const liEl = document.createElement("li");
+  liEl.innerText = element.name;
+  liEl.className = "card";
+  const imgTag = document.createElement("img");
+  imgTag.src = element.sprites.back_default;
+  liEl.append(imgTag);
+  pokCard.append(liEl);
+});
