4505b4081c94b291132dc65cb93805200d26cc45
Valentina <valentinagalyam@gmail.com>
2023-04-13 14:07:02 +0200

    some pics

--- src/index.js
+++ src/index.js
@@ -12,15 +12,24 @@ for (let i = 0; i < data.length; i++) {
     const pokemonElement = document.createElement('li')
     pokemonElement.classList.add('card')
 
+    const pokemonImage = document.createElement('img')
+    pokemonImage.classList.add('card--img')
+
     // create name as an h2
     const pokemonName = document.createElement('h2')
     pokemonName.classList.add('card--title')
     pokemonName.innerText = data[i].name
 
+    pokemonImage.setAttribute('src', data[i].sprites.other["official-artwork"].front_default)
+    pokemonElement.setAttribute('width', 256)
+
     // append h2 to li
     pokemonElement.append(pokemonName)
+    pokemonElement.append(pokemonImage)
+    
 
 
     // append li to cardlist
     cardList.append(pokemonElement)
+    // cardList.append(pokemonImage)
 }
