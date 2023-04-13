e33779d9c47bc09641b119b49a08425edebdc051
Nikita Pieters <nikita_pieters@hotmail.com>
2023-04-13 13:40:47 +0200

    tried to add the images

--- src/index.js
+++ src/index.js
@@ -1,6 +1,30 @@
-
 console.log(data);
 
-//You can start simple and just render a single 
+//You can start simple and just render a single
 //pokemon card from the first element
 console.log(data[0]);
+
+const cardList = document.querySelector(".cards");
+console.log(cardList);
+
+for (let i = 0; i < data.length; i++) {
+  const pokemonElement = document.createElement("li");
+  pokemonElement.classList.add("card");
+
+  const pokemonName = document.createElement("h2");
+  pokemonName.classList.add("card--title");
+  pokemonName.innerText = data[i].name;
+
+  const pokemonImage = document.createElement("img");
+  pokemonImage.setAttribute(
+    "src",
+    data[i].sprites.other.dream_world.front_default
+  );
+  pokemonImage.setAttribute("width", 256);
+  pokemonImage.classList.add("card--img");
+  pokemonElement.append(pokemonName);
+  pokemonElement.append(pokemonImage);
+  cardList.append(pokemonElement);
+}
+
+// link (line 21)not working for corret image data[i].sprites.other.dream_world["official-artwork"].front_default
