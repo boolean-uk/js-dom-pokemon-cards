5d157e12beacb3939f9cee3e7760dc276a76cb05
Sebastian Andrisan <sebyyyandr@gmail.com>
2023-04-13 16:36:37 +0300

    still going

--- src/index.js
+++ src/index.js
@@ -3,9 +3,6 @@ console.log(data[0]);
 
 
 const elementPokemon = document.querySelector('.card')
-//elementPokemon.innerText = "Bulnasdnisd"
-//You can start simple and just render a single 
-//pokemon card from the first element
 
 const pName = document.createElement('h2')
 pName.classList.add("card--title")
