8a7a46fbad70bf6599b6e1bf96b7d7354be89a55
carolarruda <carolinacalouroarruda@gmail.com>
2023-04-13 09:35:53 +0200

    cleaned up code

--- src/index.js
+++ src/index.js
@@ -1,8 +1,3 @@
-// console.log(data);
-// console.log(data[0]);
-
-//You can start simple and just render a single
-//pokemon card from the first element
 
 for (let i = 0; i <= 19; i++) {
   //create the outer list
@@ -10,18 +5,15 @@ for (let i = 0; i <= 19; i++) {
   const ul = document.querySelector("ul");
   li.setAttribute("class", "card");
   li.style.listStyleType = 'none';
-  /* document.getElementsById("test").style.color = red; */
-
-  // console.log(li);
-  // data[i].sprites.other['official-artwork'].front_default
   ul.append(li);
+
   // creating the Pokemon h2 title
   const h2 = document.createElement("h2");
   h2.innerText = (data[i].name).charAt(0).toUpperCase()+(data[i].name).slice(1);
   h2.setAttribute("class", "card--title");
   li.append(h2);
 
-  // create the image bit
+  // create the image
 
   const img = document.createElement("img");
   img.src = data[i].sprites.other["official-artwork"].front_default;
@@ -29,40 +21,27 @@ for (let i = 0; i <= 19; i++) {
   img.setAttribute("class", "card--img")
   img.setAttribute("width", "256")
 
+  // create the inner ul for every card
+
   const newUl = document.createElement('ul');
   newUl.setAttribute("class", "card--text");
   li.append(newUl)
 
-  const newLi = document.createElement('li');
-  newLi.innerText = (data[i].stats[0].stat.name).toLocaleUpperCase() + ": " + data[i].stats[0].base_stat
-  newUl.append(newLi)
+  // create inner list for each card
 
-  const newLiTwo = document.createElement('li');
-  newLiTwo.innerText = (data[i].stats[1].stat.name).toLocaleUpperCase() + ": " + data[i].stats[1].base_stat
-  newUl.append(newLiTwo)
+  for (j = 0; j <= 5; j++) {
+  
+    const newLi = document.createElement('li');
+  newLi.innerText = (data[i].stats[j].stat.name).toLocaleUpperCase() + ": " + data[i].stats[j].base_stat
+  newUl.append(newLi)}
 
-  const newLiThree = document.createElement('li');
-  newLiThree.innerText = (data[i].stats[2].stat.name).toLocaleUpperCase() + ": " + data[i].stats[2].base_stat
-  newUl.append(newLiThree)
 
-  const newLiFour = document.createElement('li');
-  newLiFour.innerText = (data[i].stats[3].stat.name).toLocaleUpperCase() + ": " + data[i].stats[3].base_stat
-  newUl.append(newLiFour)
-
-  const newLiFive = document.createElement('li');
-  newLiFive.innerText = (data[i].stats[4].stat.name).toLocaleUpperCase() + ": " + data[i].stats[4].base_stat
-  newUl.append(newLiFive)
-
-  const newLiSix = document.createElement('li');
-  newLiSix.innerText = (data[i].stats[5].stat.name).toLocaleUpperCase() + ": " + data[i].stats[5].base_stat
-  newUl.append(newLiSix)
+  //inner list style
 
   newUl.style.listStyleType = 'none';
   newUl.style.lineHeight = "25px";
   newUl.style.marginLeft = "10px";
 
 
-
-
 }
-console.log(data);
+
