d28b1184425426fe040296d8fb216808914c5d18
Nikolaos Giagkinis <giagkinis.nick@gmail.com>
2023-04-13 15:23:20 +0300

    Fixed name capitalization, stat spacing, changed to official artwork images

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ console.log(data);
 //You can start simple and just render a single 
 //pokemon card from the first element
 console.log(data[0]);
+
 const cardList = document.querySelector('.cards')
 for(let i = 0; i < data.length; i++) {
     const card = document.createElement('li')
@@ -11,20 +12,21 @@ for(let i = 0; i < data.length; i++) {
 
     const title = document.createElement('h2')
     title.classList.add('card--title')
-    title.innerText=data[i].name
+    const name = data[i].name.charAt(0).toUpperCase() + data[i].name.substring(1)
+    title.innerText = name
     card.append(title)
 
     const image = document.createElement('img')
     image.setAttribute('width', "256")
     image.classList.add("card--img")
-    image.setAttribute('src', data[i].sprites.front_default)
+    image.setAttribute('src', data[i].sprites.other["official-artwork"].front_default)
     card.append(image)
 
     const listOfStats = document.createElement('ul')
     listOfStats.classList.add('card--text')
     for(let j = 0; j < data[i].stats.length; j++){
         const statElementOfList = document.createElement('li')
-        statElementOfList.innerText=data[i].stats[j].stat.name.toUpperCase() + '; ' + data[i].stats[j].base_stat
+        statElementOfList.innerText=data[i].stats[j].stat.name.toUpperCase() + ': ' + data[i].stats[j].base_stat
         listOfStats.append(statElementOfList)
     }
     card.append(listOfStats)
