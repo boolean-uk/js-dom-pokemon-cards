d4db17b1aa13815409d18d06221b9c654ce5c894
Aidan van Geest <aidanvangeest@gmail.com>
2023-04-13 13:16:52 +0200

    Appended image

--- src/index.js
+++ src/index.js
@@ -11,6 +11,14 @@ const heading = document.createElement("h2");
 heading.classList.add("card--title");
 heading.innerText = "Bulbasaur";
 newCard.append(heading);
+const image = document.createElement('img')
+image.classList.add('card--img')
+image.setAttribute('src', 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png')
+image.setAttribute('width', 256)
+newCard.append(image)
+const text = document.createElement('ul')
+text.classList.add('card--text')
+
 // newCard.innerText = "Bulbasaur"
 
 cards.append(newCard);
