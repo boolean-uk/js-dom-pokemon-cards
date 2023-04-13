7efc6bc4081ebbb51be897a140094bbcc516001c
Koci H <husnukoci94@gmail.com>
2023-04-14 00:32:00 +0300

    Extensions are done

--- src/index.js
+++ src/index.js
@@ -1,43 +1,63 @@
-// console.log(data);
-// console.log(data[0]);
 
-//You can start simple and just render a single
+for (i = 0; i < data.length; i++) {
+    const ul = document.querySelector('ul');
+    const li = document.createElement('li');
+    li.setAttribute('class', 'card');
+    li.style.listStyleType = 'none';
+    ul.append(li);
+
+    const h2 = document.createElement('h2');
+    h2.setAttribute('class', 'card--title');
+    h2.innerText = data[i].name.charAt(0).toUpperCase() + data[i].name.slice(1)
+    li.append(h2);
+
+    const img = document.createElement('img')
+    img.setAttribute('class', 'card--img');
+    li.append(img);
+    img.setAttribute('width', '256');
+    img.src = data[i].sprites.other["official-artwork"].front_default;
+    li.append(img);
+
+    const newUl = document.createElement('ul');
+    newUl.setAttribute('class', 'card--text')
+
+    li.append(newUl)
+    
+    for (j = 0; j < data[j].stats.length; j++) {
+    const newLi = document.createElement('li');
+    newLi.innerText = data[i].stats[j].stat.name.toUpperCase() + ": " + data[i].stats[j].base_stat;
+    newLi.style.listStyleType = 'none';
+    newUl.append(newLi);
+    }
+    const newLiTwo = document.createElement('li');
+    const arr = ['GAMES APPEARED: ', Math.floor(Math.random() * 100)];
+    const str = arr.join("");
+    newLiTwo.innerText = str;
+    newLiTwo.style.listStyleType = 'none';
+    newUl.append(newLiTwo);
+
+    
+
+
+    let toggle = true;
+    img.addEventListener('click', function(){
+    toggle = !toggle;
+    if(toggle){
+        img.src = 'img//one.png'; 
+    }else{
+        img.src = 'img//two.jpg';
+    }
+    })
+}
+
+
+
+
+
+
+
+
+
+//You can start simple and just render a single 
 //pokemon card from the first element
 
-for (let i = 0; i < data.length; i++) {
-  //create the outer list
-  const li = document.createElement("li");
-  const ul = document.querySelector("ul");
-  li.setAttribute("class", "card");
-  li.style.listStyleType = 'none';
-  /* document.getElementsById("test").style.color = red; */
-
-  // console.log(li);
-  // data[i].sprites.other['official-artwork'].front_default
-  ul.append(li);
-  // creating the Pokemon h2 title
-  const h2 = document.createElement("h2");
-  h2.innerText = (data[i].name).charAt(0).toUpperCase()+(data[i].name).slice(1);
-  h2.setAttribute("class", "card--title");
-  li.append(h2);
-
-  // create the image bit
-
-  const img = document.createElement("img");
-  img.src = data[i].sprites.other["official-artwork"].front_default;
-  li.append(img);
-  img.setAttribute("class", "card--img")
-  img.setAttribute("width", "256")
-
-  const newUl = document.createElement('ul');
-  newUl.setAttribute("class", "card--text");
-  li.append(newUl)
-
-  for (j = 0; j < data[j].stats.length; j++) {
-    const newLi = document.createElement('li');
-    newLi.innerText = (data[i].stats[j].stat.name).toLocaleUpperCase() + ": " + data[i].stats[j].base_stat
-    newUl.append(newLi)
-    newUl.style.listStyleType = 'none';
-  }
-}
-console.log(data);
