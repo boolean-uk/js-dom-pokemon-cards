b874e3a47039aed374631a217490c984e5686543
Carmen Barcia <carmen.barcia@hotmail.com>
2023-04-13 16:46:10 +0200

    style is missing

--- src/index.js
+++ src/index.js
@@ -1,11 +1,20 @@
 
 const cards = document.querySelector('.cards')
+const pCards = document.createElement('ul')
+pCards.classList.add("cards")
 
 for (let j=0; j < data.length; j++) {
     const elementPokemon = document.querySelector('.card')
+    const firstPokemon = document.createElement('li')
+    firstPokemon.classList.add("card")
     cards.appendChild(elementPokemon)
+    pCards.appendChild(firstPokemon)
+
+
     const pName = document.createElement('h2')
-    pName.classList.add("card--title")
+
+    const cardtitle = document.querySelector('.card--text')
+    pName.classList.add("cardtitle")
     pName.innerText = data[j].name
     
     elementPokemon.append(pName)
@@ -20,8 +29,9 @@ for (let j=0; j < data.length; j++) {
     
     for(let i=0; i<= 5; i++) {
         const stat = document.createElement('li')
-        stat.append(document.createTextNode(data[j].stats[i].stat.name + data[j].stats[i].base_stat))
+        stat.append(data[j].stats[i].stat.name + data[j].stats[i].base_stat)
         text.appendChild(stat)
         elementPokemon.append(text)
     }
+   
 }
