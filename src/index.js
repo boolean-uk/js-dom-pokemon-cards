1145ac8307bf0074abf5275c57c73c0bac55a21a
carolarruda <carolinacalouroarruda@gmail.com>
2023-04-14 11:50:43 +0200

    toggle images done

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 
-for (let i = 0; i <= 19; i++) {
+for (let i = 0; i <= data.length; i++) {
   //create the outer list
+  const pokemon = data[i];
   const li = document.createElement("li");
   const ul = document.querySelector("ul");
   li.setAttribute("class", "card");
@@ -9,17 +10,32 @@ for (let i = 0; i <= 19; i++) {
 
   // creating the Pokemon h2 title
   const h2 = document.createElement("h2");
-  h2.innerText = (data[i].name).charAt(0).toUpperCase()+(data[i].name).slice(1);
+  h2.innerText = pokemon.name.charAt(0).toUpperCase()+(pokemon.name).slice(1);
   h2.setAttribute("class", "card--title");
   li.append(h2);
 
   // create the image
 
   const img = document.createElement("img");
-  img.src = data[i].sprites.other["official-artwork"].front_default;
+  img.src = pokemon.sprites.other["official-artwork"].front_default;
   li.append(img);
   img.setAttribute("class", "card--img")
   img.setAttribute("width", "256")
+
+  // extension with click event that toggles between images
+
+  let toggle = true;
+  
+  img.addEventListener("click", function event () {
+    toggle = !toggle
+    if (toggle) {
+      img.src = pokemon.sprites.other["dream_world"].front_default;
+  
+    } else {
+      img.src = pokemon.sprites.other["official-artwork"].front_default;
+    }
+    
+})
 
   // create the inner ul for every card
 
@@ -32,7 +48,7 @@ for (let i = 0; i <= 19; i++) {
   for (j = 0; j <= 5; j++) {
   
     const newLi = document.createElement('li');
-  newLi.innerText = (data[i].stats[j].stat.name).toLocaleUpperCase() + ": " + data[i].stats[j].base_stat
+  newLi.innerText = (pokemon.stats[j].stat.name).toLocaleUpperCase() + ": " + pokemon.stats[j].base_stat
   newUl.append(newLi)}
 
 
