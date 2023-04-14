7f82ce729ae886f6bf9cd2e9a85da23181cfdec1
Isabelle <isabel.tartarelli@live.it>
2023-04-14 15:05:23 +0100

    Completed core

--- src/index.js
+++ src/index.js
@@ -9,58 +9,73 @@ const pokCard = document.querySelector(".cards");
 data.forEach((element) => {
   const pokName = document.createElement("h2");
   pokName.innerText = element.name;
-  pokName.className = "card--title"
+  pokName.className = "card--title";
 
   const liEl = document.createElement("li");
   liEl.className = "card";
 
   const imgTag = document.createElement("img");
-  imgTag.src = element.sprites.front_default;
-  imgTag.className = "card--img"
-
-  const ulStat = document.createElement("ul")
-  ulStat.className="card--text"
-
-
-  const pokHp = document.createElement("li");
-  pokHp.innerText = element.stats[0].stat.name;
-  pokHp.className = "card--text";
-
-  const pokStats = document.createElement("li");
-  pokStats.innerText = element.stats[0].base_stat;
-  
-
-  const pokAttack = document.createElement("li");
-  pokAttack.innerText = element.stats[1].stat.name;
-
-
-  const pokAttackNum = document.createElement("li");
-  pokAttackNum.innerText = element.stats[1].base_stat;
-
-
-  const pokDefense = document.createElement("li");
-  pokDefense.innerText = element.stats[2].stat.name;
+  imgTag.src = element.sprites.other["official-artwork"].front_default;
+  imgTag.className = "card--img";
+  imgTag.setAttribute("width", "256")
  
 
-  const pokDefNum = document.createElement("li");
-  pokDefNum.innerText = element.stats[2].base_stat;
+  const ulStat = document.createElement("ul");
+  ulStat.className = "card--text";
+
+  const pokHp = document.createElement("li");
+  pokHp.innerText = element.stats[0].stat.name.toString().toUpperCase();
+  const pokStats = element.stats[0].base_stat;
+ 
+
+  const pokAttack = document.createElement("li");
+  pokAttack.innerText = element.stats[1].stat.name.toString().toUpperCase();;
+  const pokAttackNum = element.stats[1].base_stat;
   
+
+  const pokDefense = document.createElement("li");
+  pokDefense.innerText = element.stats[2].stat.name.toString().toUpperCase();;
+  const pokDefNum = element.stats[2].base_stat;
+  
+
+  const pokSpecAttack = document.createElement("li");
+  pokSpecAttack.innerText = element.stats[3].stat.name.toString().toUpperCase();;
+  const pokSAttNum =  element.stats[3].base_stat;
+  
+
+  const pokSpecDefence = document.createElement("li");
+  pokSpecDefence.innerText = element.stats[4].stat.name.toString().toUpperCase();;
+
+  const pokSDefNum = element.stats[4].base_stat;
+
+
+  const pokSpeed = document.createElement("li");
+  pokSpeed.innerText = element.stats[5].stat.name.toString().toUpperCase();;
+  const pokSpeedNum = element.stats[5].base_stat;
 
 
   liEl.append(pokName);
   liEl.append(imgTag);
 
-  liEl.append(ulStat)
+  liEl.append(ulStat);
 
-  pokStats.append(pokHp);
-  ulStat.append(pokStats);
+  ulStat.append(pokHp);
+  pokHp.append(": ", pokStats);
 
- ulStat.append(pokAttackNum);
-  pokAttackNum.append(pokAttack);
- 
+  ulStat.append(pokAttack);
+  pokAttack.append(": ", pokAttackNum);
 
-  pokDefNum.append(pokDefense);
-  ulStat.append(pokDefNum);
+  ulStat.append(pokDefense);
+  pokDefense.append(": ", pokDefNum);
+
+  ulStat.append(pokSpecAttack);
+  pokSpecAttack.append(": ", pokSAttNum);
+
+  ulStat.append(pokSpecDefence);
+  pokSpecDefence.append(": ", pokSDefNum);
+
+  ulStat.append(pokSpeed);
+  pokSpeed.append(": ", pokSpeedNum);
 
   pokCard.append(liEl);
 });
