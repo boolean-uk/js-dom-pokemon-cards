b6eb0821f6aa491ae8104db0dc9dd5edde2b18cd
ks46 <konastaf46@gmail.com>
2023-08-14 13:25:56 +0300

    Render a single pokemon card
    * Implemented drawCard() function that renders a single pokemon card
    * Changed the script paths to a relative path in index.html

--- src/index.js
+++ src/index.js
@@ -1,6 +1,37 @@
+const container = document.querySelector('.cards')
 
-console.log(data);
+function capitalise(str) {
+  return str.charAt(0).toUpperCase() + str.slice(1)
+}
 
-//You can start simple and just render a single 
-//pokemon card from the first element
-console.log(data[0]);
+function drawCard(data) {  // render a single pokemon card
+  // create card element
+  const card = document.createElement('li')
+  card.classList.add('card')
+  // create title of card
+  const title = document.createElement('h2')
+  title.innerText = capitalise(data.name)
+  title.classList.add('card--title')
+  card.append(title)
+  // create image of card
+  const image = document.createElement('img')
+  image.style.width = '256'
+  image.classList.add('card--img')
+  image.src = data.sprites.other['official-artwork']['front_default']
+  card.append(image)
+  // create list of stats of card
+  const text = document.createElement('ul')
+  text.classList.add('card--text')
+  for (let i = 0; i < data.stats.length; ++i) {
+    // create stats of card
+    const stats = document.createElement('li')
+    const elem = data.stats[i]
+    stats.innerHTML = elem.stat.name.toUpperCase() + ': ' + elem.base_stat
+    text.append(stats)
+  }
+  card.append(text)
+
+  container.append(card)
+}
+
+drawCard(data[0])  // experiment by rendering first card of cards array
