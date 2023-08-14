a88415bede06e13930fc2aa7d5e35d1d72e69958
ks46 <konastaf46@gmail.com>
2023-08-14 17:40:34 +0300

    Extention Part 1

--- src/index.js
+++ src/index.js
@@ -8,27 +8,47 @@ function drawCard(data) {  // render a single pokemon card
   // create card element
   const card = document.createElement('li')
   card.classList.add('card')
+
   // create title of card
   const title = document.createElement('h2')
   title.innerText = capitalise(data.name)
   title.classList.add('card--title')
   card.append(title)
+
   // create image of card
   const image = document.createElement('img')
   image.style.width = '256px'
   image.classList.add('card--img')
   image.src = data.sprites.other['official-artwork']['front_default']
   card.append(image)
-  // create list of stats of card
+
   const text = document.createElement('ul')
+  // create list of stats of card
   text.classList.add('card--text')
-  for (let i = 0; i < data.stats.length; ++i) {
-    // create stats of card
+  data.stats.forEach(element => {
     const stats = document.createElement('li')
-    const elem = data.stats[i]
-    stats.innerHTML = elem.stat.name.toUpperCase() + ': ' + elem.base_stat
+    stats.innerHTML = element.stat.name.toUpperCase() + ': ' + element.base_stat
     text.append(stats)
-  }
+  })
+  // create list of appearances of card
+  const appearances = document.createElement('li')
+  let currentGameIndex = null
+  let appearancesText = ''
+  let printDelimeter = false
+  data.game_indices.forEach(element => {
+    if (element.game_index !==  currentGameIndex) {
+      currentGameIndex = element.game_index
+      appearancesText += `\nAPPEARS IN GAME #${currentGameIndex}: `
+      printDelimeter = false
+    }
+    if (printDelimeter) {
+      appearancesText += ', '
+    }
+    appearancesText += element.version.name
+    printDelimeter = true
+  })
+  appearances.innerHTML = appearancesText
+  text.append(appearances)
   card.append(text)
 
   container.append(card)
@@ -38,5 +58,4 @@ function drawCards(cardsArray) {
   cardsArray.forEach(element => drawCard(element));
 }
 
-// drawCard(data[0])  // experiment by rendering first card of cards array
 drawCards(data)
