50a7d3d074ed07f6c32c6182712c99b4541c09e7
Panagiotis Karapiperis <panagiotis_karapiperis@hotmail.com>
2023-08-14 13:58:28 +0300

    Panagiotis Karapiperis js-dom-pokemon

--- src/index.js
+++ src/index.js
@@ -1,6 +1,35 @@
-
-console.log(data);
+const cards = document.querySelector('.cards')
 
 //You can start simple and just render a single 
 //pokemon card from the first element
-console.log(data[0]);
+
+for (let i = 0; i < data.length; i++) {
+    const card = document.createElement('li')
+    card.classList.add('card')
+
+    const cardTitle = document.createElement('h2')
+    cardTitle.classList.add('card--title')
+    cardTitle.innerText = data[i].name
+    card.append(cardTitle)
+
+    const img = document.createElement('img')
+    img.width = 256
+    img.classList.add("card--img")
+    img.src = data[i].sprites.other["official-artwork"].front_default
+    card.append(img)
+    
+    const cardText = document.createElement('ul')
+    cardText.classList.add("card--text")
+
+    for (let j = 0; j < data[i].stats.length; j++) {
+        const stat = document.createElement('li')
+        stat.innerText = data[i].stats[j].stat.name.toUpperCase() + ': ' + data[i].stats[j].base_stat
+        cardText.append(stat)
+    }
+
+    card.append(cardText)
+    cards.append(card)
+}
+
+console.log(data[0].stats);
+console.log(cards);
