462356d6cc8eaff1a1499e6a2ad1f84d1d7d803c
ks46 <konastaf46@gmail.com>
2023-08-15 15:06:21 +0300

    Extention Part 2
    * Implemented carousel functionality
    * First image does not load properly

--- src/index.js
+++ src/index.js
@@ -1,7 +1,77 @@
 const container = document.querySelector('.cards')
+const slideIndices = {}   // key: slideIndex, value: slideNo
+for (const [key, value] of Object.entries(slideIndices)) {
+  showSlides(key, value)
+}
 
 function capitalise(str) {
   return str.charAt(0).toUpperCase() + str.slice(1)
+}
+
+function showSlides(slideNo, slideIndex) {
+  let slides = document.getElementsByClassName(`mySlides${slideIndex}`)
+
+  if (slideNo > slides.length) {
+    slideNo = 1
+  } else if (slideNo < 1) {
+    slideNo = slides.length
+  }
+  slideIndices[slideIndex] = slideNo
+
+  // console.log(`slideIndices[${slideIndex}] = ${slideIndices[slideIndex]}, slideNo = ${slideNo}, slides.length = ${slides.length}`)    // TODO: delete this statement
+
+  for (let i = 0; i < slides.length; ++i) {
+    slides[i].style.display = 'none'
+  }
+  slides[slideNo-1].style.display = 'block'
+}
+
+function drawSlideshow(data) {
+  // append index of element to list of indices
+  const slideIndex = data.id
+  slideIndices[slideIndex] = 1
+  const slideshowContainer = document.createElement('div')
+  slideshowContainer.classList.add('slideshow-container')
+  // array of image links to display
+  const imageLinks = [
+    data.sprites.other['official-artwork']['front_default']
+    , data.sprites.other.dream_world.front_default
+    // , data.sprites.versions["generation-iii"]["emerald"].front_default
+  ]
+
+  // create div for each image
+  imageLinks.forEach(element => {
+    const imageDiv = document.createElement('div')
+    imageDiv.classList.add('mySlides', `mySlides${slideIndex}`, 'fade')
+    // create image of slideshow
+    const image = document.createElement('img')
+    image.style.width = '256px'
+    image.classList.add('card--img')
+    image.src = element
+    imageDiv.append(image)
+    slideshowContainer.append(imageDiv)
+  })
+  // create previous and next buttons
+  const buttons = ['prev', 'next']
+  buttons.forEach((element, index) => {
+    const button = document.createElement('a')
+    button.classList.add(element)
+    const isPrevButton = index === 0
+    if (index === 0) {  // implement previous button
+      button.innerHTML = `&#10094;`
+      button.onclick = function decrementSlides() {
+          showSlides(slideIndices[slideIndex] - 1, slideIndex)
+        }
+    } else {  // implement next button
+      button.innerHTML = `&#10095;`
+      button.onclick = function incrementSlides() {
+          showSlides(slideIndices[slideIndex] + 1, slideIndex)
+        }
+    }
+    slideshowContainer.append(button)
+  })
+  
+  return slideshowContainer
 }
 
 function drawCard(data) {  // render a single pokemon card
@@ -15,12 +85,9 @@ function drawCard(data) {  // render a single pokemon card
   title.classList.add('card--title')
   card.append(title)
 
-  // create image of card
-  const image = document.createElement('img')
-  image.style.width = '256px'
-  image.classList.add('card--img')
-  image.src = data.sprites.other['official-artwork']['front_default']
-  card.append(image)
+  // create slideshow of images
+  const slideshow = drawSlideshow(data)
+  card.append(slideshow)
 
   const text = document.createElement('ul')
   // create list of stats of card
@@ -33,12 +100,12 @@ function drawCard(data) {  // render a single pokemon card
   // create list of appearances of card
   const appearances = document.createElement('li')
   let currentGameIndex = null
-  let appearancesText = ''
+  let appearancesText = 'APPEARS IN:'
   let printDelimeter = false
   data.game_indices.forEach(element => {
     if (element.game_index !==  currentGameIndex) {
       currentGameIndex = element.game_index
-      appearancesText += `\nAPPEARS IN GAME #${currentGameIndex}: `
+      appearancesText += ` GAME #${currentGameIndex}: `
       printDelimeter = false
     }
     if (printDelimeter) {
