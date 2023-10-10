36468c8ce88f55609b38fa7d4e24145d91f7a254
Nazar Tymiv <nazartumiv@gmail.com>
2023-10-10 22:44:34 +0100

    Project has been finished

--- src/index.js
+++ src/index.js
@@ -1,6 +1,41 @@
+const cards = document.querySelector(".cards");
 
-console.log(data);
+data.forEach((pokemon) => {
+    const name = pokemon.name;
+    const photo1 = pokemon.sprites.other["official-artwork"].front_default;
+    const photo2 = pokemon.sprites.other.dream_world.front_default;
+    const hp = pokemon.stats.find((i) => i.stat.name === "hp").base_stat;
+    const attack = pokemon.stats.find(
+        (i) => i.stat.name === "attack"
+    ).base_stat;
+    const defense = pokemon.stats.find(
+        (i) => i.stat.name === "defense"
+    ).base_stat;
+    const special_attack = pokemon.stats.find(
+        (i) => i.stat.name === "special-attack"
+    ).base_stat;
+    const special_defense = pokemon.stats.find(
+        (i) => i.stat.name === "special-defense"
+    ).base_stat;
+    const speed = pokemon.stats.find((i) => i.stat.name === "speed").base_stat;
 
-//You can start simple and just render a single 
-//pokemon card from the first element
-console.log(data[0]);
+    const element = `
+        <li class="card">
+            <h2 class="card--title">${name}</h2>
+            <img width="256" class="card--img"
+                src="${photo1}" />
+            <img width="256" class="card--img card--img-another"
+                src="${photo2}" />
+            <ul class="card--text">
+                <li>HP: ${hp}</li>
+                <li>ATTACK: ${attack}</li>
+                <li>DEFENSE: ${defense}</li>
+                <li>SPECIAL-ATTACK: ${special_attack}</li>
+                <li>SPECIAL-DEFENSE: ${special_defense}</li>
+                <li>SPEED: ${speed}</li>
+            </ul>
+        </li>
+    `;
+
+    cards.innerHTML += element;
+});
