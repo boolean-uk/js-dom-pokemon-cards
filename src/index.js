a980626e9c88e08316b7592c9877e3f302cadfc5
Jack Miller <jdmiller1991@outlook.com>
2023-10-10 16:24:28 +0100

    Initial draft

--- src/index.js
+++ src/index.js
@@ -1,6 +1,42 @@
+const pokemonCardList = document.querySelectorAll('.cards');
+
+
+
+
+
+
+// Card Structure
+
+const pokemonCard = document.createElement('li');
+pokemonCard.style.listStyleType = 'none'
+pokemonCard.setAttribute('class', '.card');
+pokemonCardList.append(pokemonCard);
+
+// Card Title
+
+const pokemonCardTitle = document.createElement('h2');
+pokemonCardTitle.setAttribute('class', 'h2');
+pokemonCardList.append(pokemonCardTitle);
+
+// Card Image
+
+const pokemonCardImage = document.createElement('img');
+pokemonCardImage.setAttribute('class', '.card--img', 'img')
+pokemonCardImage.setAttribute('src', data.sprites.front_default);
+pokemonCardList.append(pokemonCardImage);
+
+// Card Stats
+
+const pokemonCardStats = document.createElement('ul');
+pokemonCard.style.listStyleType = 'none'
+pokemonCard.setAttribute('class', '.card--text');
+pokemonCardList.append(pokemonCardStats);
+
+
+
 
 console.log(data);
 
-//You can start simple and just render a single 
-//pokemon card from the first element
+
+
 console.log(data[0]);
