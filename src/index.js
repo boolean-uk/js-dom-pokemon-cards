9bb1b78a88f3eddd7793735078dda411621a206a
Pierluigi <pierluigi.capirci89@gmail.com>
2023-10-10 15:27:07 +0100

    iterating data

--- src/index.js
+++ src/index.js
@@ -1,5 +1,17 @@
-
 console.log(data);
+const container = document.querySelector(".cards");
+
+function capitalize(string) {
+  return string.charAt(0).toUpperCase() + string.slice(1);
+}
+
+data.forEach((pokemon) => {
+    const h2 = document.createElement("h2");
+    h2.setAttribute("class", "card--title");
+    h2.innerText = capitalize(pokemon.name);
+    container.append(h2);
+}
+);
 
 //You can start simple and just render a single 
 //pokemon card from the first element
