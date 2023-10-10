906e13c22ddd0d0f26655a86146966a06328efe7
callumhayden <callum.hayden@gmail.com>
2023-10-10 19:49:11 +0100

    more added to core

--- src/index.js
+++ src/index.js
@@ -10,40 +10,39 @@ for (let i = 0; i < data.length; i++) {
 
 const pokemon = data[i]
     const pokemonBeast = document.createElement("li");
-    list.append(pokemonBeast)
+    list.append(pokemonBeast);
 
     const attributeList = document.createElement("ul");
-    pokemonBeast.setAttribute("class", "card")
+    pokemonBeast.classList.add("class", "card")
 
     const header = document.createElement("h2");
-    pokemonBeast.append(header)
+    pokemonBeast.append(header);
 
-    header.setAttribute("class", "card--title")
+    header.setAttribute("class", "card--title");
     header.innerText = pokemon.name
     header.style.textTransform = "capitalize"
-    attributeList.setAttribute("class", "card--text")
+    attributeList.classList.add("class", "card--text");
     attributeList.style.listStyle = "none"
     attributeList.style.lineHeight = "1.8"
 
 
     const image = document.createElement("img");
-    pokemonBeast.append(image)
-    pokemonBeast.append(attributeList)
+    pokemonBeast.append(image);
+    pokemonBeast.append(attributeList);
     pokemonBeast.style.listStyle = "none"
     
-    image.setAttribute("class", "card--img")
+    image.setAttribute("class", "card--img");
     image.src = pokemon.sprites.other["official-artwork"].front_default
     
 
     for (let a = 0; a < pokemon.stats.length; a++) {
-        const attributeName = pokemon.stats[a].stat.name
-        const attributeValue = pokemon.stats[a].base_stat
+        const attribute = pokemon.stats[a].stat.name
+        const attributeLevel = pokemon.stats[a].base_stat
         const attributeListItem = document.createElement("li")
-        attributeListItem.innerText = `${attributeName}: ${attributeValue}`.toUpperCase()
+        attributeListItem.innerText = `${attribute}: ${attributeLevel}`.toUpperCase()
         attributeList.append(attributeListItem)
     }
 
- const background = document.c
 
 
 }
