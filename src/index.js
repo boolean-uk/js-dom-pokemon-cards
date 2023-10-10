7794c2b7a0eea3d37ef7cfa0cff45bc77695c164
Chloe Zermatten <chloezermatten@gmail.com>
2023-10-10 17:10:38 +0100

    .forEach() implemented - all cards generated

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 
 console.log(data);
 
+data.forEach (element => {
+
 //Create the card (li element which will contain all others)
 const list = document.querySelector('ul')
 const card = document.createElement('li')
@@ -10,40 +12,49 @@ list.append(card)
 
 //get the name from data.js, assign it to a variable, append it to card.
 const pokemonName = document.createElement('h2')
-    pokemonName.innerText = `${data[0].name}`
+    pokemonName.innerText = `${data[data.indexOf(element)].name}`
+    pokemonName.setAttribute('class', 'card--title')
 card.append(pokemonName)
 
 //get the name from data.js, assign it to a variable, append it to card.
 
 
 const image = document.createElement('img')
-//     image.src = `${data[0].sprites.other[1].front_default}`
+image.setAttribute('class','card--img')
+// image.setAttribue('src', data[0].sprites.others[1].front_default)
 card.append(image)
 
 
 //create and append the stats ul with all its li 
 const stats = document.createElement('ul')
+stats.setAttribute('class', 'card--text')
 card.append(stats)
 
 
 const hp = document.createElement('li')
 stats.append(hp)
+hp.innerText = 'hp'
 
 const attack = document.createElement('li')
 stats.append(attack)
+attack.innerText = 'attack'
 
 const defense = document.createElement('li')
 stats.append(defense)
+defense.innerText = 'defense'
 
 const specialAttack = document.createElement('li')
 stats.append(specialAttack)
+specialAttack.innerText = 'specialAttack'
 
 const specialDefense = document.createElement('li')
 stats.append(specialDefense)
+specialDefense.innerText = 'specialDefense'
 
 const speed= document.createElement('li')
 stats.append(speed)
+speed.innerText = 'speed'
 
-
+})
 
 console.log(data[0]);
