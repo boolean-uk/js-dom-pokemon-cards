bc811f7a639d2790b87dff1b589bd84dd776bd88
Lukas Dembicki <lad_777@hotmail.com>
2023-10-10 16:38:27 +0100

    working on single card. Almost.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,39 @@
-
 console.log(data);
 
-//You can start simple and just render a single 
+//You can start simple and just render a single
 //pokemon card from the first element
 console.log(data[0]);
+
+const cards = document.querySelector(".cards");
+
+// data.forEach((value) => {
+//     const pokeName = document.createElement('h2')
+//     const listItem = document.createElement('li')
+//     listItem.append(cards)
+//     listItem.setAttribute("card")
+//     pokeName.innerHTML = `${value.name}`
+//     pokeName.setAttribute("card--title")
+// })
+
+const listItem = document.createElement("li");
+listItem.setAttribute("class", "card");
+cards.append(listItem);
+const card = document.querySelector(".card");
+
+const h2 = document.createElement("h2");
+h2.setAttribute("class", "card--title");
+h2.innerText = `bulbasaur`;
+card.append(h2);
+
+const img = document.createElement("img");
+img.setAttribute("class", "card--img");
+img.setAttribute("width", "256");
+img.setAttribute(
+    "src",
+    "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png"
+);
+card.append(img);
+
+const ul = document.createElement("ul");
+ul.setAttribute("class", "card--text");
+
