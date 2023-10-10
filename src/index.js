108707bb56e436858adfca569db8bf82e1a234ba
Satoki <satokiito@gmail.com>
2023-10-10 19:18:53 +0100

    Extension 1 complete

--- src/index.js
+++ src/index.js
@@ -16,21 +16,27 @@ data.forEach(pokemon => {
     const pokemonName = document.createElement('h2');
     const pokemonImage = document.createElement('img');
     const pokemonStats = document.createElement('ul');
+    const versionHeader = document.createElement('h3')
+    const pokemonVersions = document.createElement('ul')
 
     // UPDATE INFO
     pokemonName.innerText = pokemon.name;
     pokemonImage.src = pokemon.sprites.other["official-artwork"].front_default;
+    versionHeader.innerText = 'Games Featured In:'
 
     // UPDATE CLASSES
     individualCard.setAttribute('class', 'card');
     pokemonName.setAttribute('class', 'card--title');
     pokemonImage.setAttribute('class', 'card--img');
+
     pokemonStats.setAttribute('class', 'card--text');
+
+    versionHeader.setAttribute('class', 'card--title')
+    pokemonVersions.setAttribute('class', 'card--text');
 
     // STATS
     pokemon.stats.forEach(value => {
         const singleStat = document.createElement('li');
-        console.log(singleStat)
         singleStat.innerText = `${value.stat.name}: ${value.base_stat}`;
         pokemonStats.append(singleStat);
     })
@@ -38,13 +44,29 @@ data.forEach(pokemon => {
     // OTHER STYLING
     pokemonName.style.textTransform = 'capitalize'
     pokemonImage.width = '256'
+
     pokemonStats.style.listStyle = 'none'
     pokemonStats.style.textTransform = 'uppercase'
+
+    versionHeader.style.textAlign = 'center'
+    pokemonVersions.style.listStyle = 'none'
+    pokemonVersions.style.textTransform = 'uppercase'
+    pokemonVersions.style.textAlign = 'center'
+
+    // VERSIONS
+    pokemon.game_indices.forEach(index => {
+        const versionItems = document.createElement('li')
+        versionItems.innerText = index.version.name
+
+        pokemonVersions.append(versionItems)
+    })
 
     // APPEND TO INDIVIDUAL CARDS
     individualCard.append(pokemonName);
     individualCard.append(pokemonImage);
     individualCard.append(pokemonStats);
+    individualCard.append(versionHeader)
+    individualCard.append(pokemonVersions)
 
     // APPEND TO CARD GRID
     pokemonCardGrid.append(individualCard);
