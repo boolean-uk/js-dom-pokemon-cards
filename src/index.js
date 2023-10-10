cdda63a75dd15f7e9cf94796d815d615b20b5562
Chloe Zermatten <chloezermatten@gmail.com>
2023-10-10 18:37:28 +0100

    images added

--- src/index.js
+++ src/index.js
@@ -3,60 +3,61 @@ console.log(data);
 
 data.forEach (element => {
 
-//Create the card (li element which will contain all others)
+//CARD
 const list = document.querySelector('ul')
 const card = document.createElement('li')
 card.setAttribute('class','card')
 list.append(card)
 
 
-//get the name from data.js, assign it to a variable, append it to card.
+//NAME
 const pokemonName = document.createElement('h2')
-    pokemonName.innerText = `${data[data.indexOf(element)].name}`
+    pokemonName.innerText = `${element.name}`
     pokemonName.setAttribute('class', 'card--title')
 card.append(pokemonName)
 
-//get the name from data.js, assign it to a variable, append it to card.
-
+//IMAGE
 
 const image = document.createElement('img')
 image.setAttribute('class','card--img')
 
 image.setAttribute('width', '256')
 
-image.setAttribute('src', `${data[data.indexOf(element)]}`)
+image.setAttribute('src', `${element.sprites.other["official-artwork"].front_default}`)
 card.append(image)
 
 
-//create and append the stats ul with all its li 
+//STATS
 const stats = document.createElement('ul')
 stats.setAttribute('class', 'card--text')
 card.append(stats)
 
-
 const hp = document.createElement('li')
 stats.append(hp)
-hp.innerText = `hp: ${data[data.indexOf(element)].stats[0].base_stat}`
+hp.innerText = `hp: ${element.stats[0].base_stat}`
 
 const attack = document.createElement('li')
 stats.append(attack)
+
+
+// this is where I realised I needed a short break. 'data[data.indexOf(element)]', seriously!? I corrected the other instances (yes, this was the norm at one point, but though I'd just leave one out there as a hopefuly somewhat amusing wee reminder)
 attack.innerText = `attack: ${data[data.indexOf(element)].stats[1].base_stat}`
 
 const defense = document.createElement('li')
 stats.append(defense)
-defense.innerText = `defense: ${data[data.indexOf(element)].stats[2].base_stat}`
+defense.innerText = `defense: ${element.stats[2].base_stat}`
 
 const specialAttack = document.createElement('li')
 stats.append(specialAttack)
-specialAttack.innerText = `specialAttack: ${data[data.indexOf(element)].stats[3].base_stat}`
+specialAttack.innerText = `specialAttack: ${element.stats[3].base_stat}`
 
 const specialDefense = document.createElement('li')
 stats.append(specialDefense)
-specialDefense.innerText = `specialDefense ${data[data.indexOf(element)].stats[4].base_stat}`
+specialDefense.innerText = `specialDefense ${element.stats[3].base_stat}`
 
 const speed= document.createElement('li')
 stats.append(speed)
-speed.innerText = `speed: ${data[data.indexOf(element)].stats[4].base_stat}`
+speed.innerText = `speed: ${element.stats[5].base_stat}`
 
 })
 
