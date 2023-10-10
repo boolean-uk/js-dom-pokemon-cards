4eadcaf344d6194bd948083c678e72aa445bb29f
TomEastwood <t.eastwood99@gmail.com>
2023-10-10 17:23:29 +0100

    all extensions completed

--- src/index.js
+++ src/index.js
@@ -8,14 +8,6 @@ function pokeTitle(pokemon) {
     return cardTitle;
 };
 
-function pokeImg(pokemon) {
-    const cardImg = document.createElement('img');
-    cardImg.setAttribute('class', 'card--img');
-    cardImg.src = `${pokemon.sprites.other['official-artwork'].front_default}`;
-    cardImg.style.width = '250px';
-    return cardImg;
-}
-
 function pokeStats(pokemon) {
     const cardStats = document.createElement('ul');
     cardStats.setAttribute('class', 'card--text');
@@ -45,19 +37,32 @@ function pokeGen(pokemon) {
     return cardGen;
 }
 
+function imageSwitch(pokemon) {
+    const originalImg = document.createElement('img');
+    const newImg = document.createElement('img');
+    originalImg.setAttribute('class', 'card--img');
+    newImg.setAttribute('class', 'card--img');
+    originalImg.src = `${pokemon.sprites.other['official-artwork'].front_default}`;
+    newImg.src = `${pokemon.sprites.other.dream_world.front_default}`;
+    originalImg.style.width = '250px';
+    newImg.style.width = '250px';
+    originalImg.addEventListener('mouseover', () => {
+        originalImg.replaceWith(newImg);
+    });
+    newImg.addEventListener('mouseout', () => {
+        newImg.replaceWith(originalImg);
+    });
+    return originalImg;
+}
+
 
 data.forEach((pokemon) => {
     const div = document.createElement('div');
     div.setAttribute('class', 'card');
     div.append(pokeTitle(pokemon));
-    div.append(pokeImg(pokemon));
+    div.append(imageSwitch(pokemon));
     div.append(pokeStats(pokemon));
     div.append(genTitle(pokemon));
     div.append(pokeGen(pokemon));
     cardContainer[0].append(div);
 });
-
-
-
-
-
