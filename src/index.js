4997b5b52d4fb6ddd3cd18ed0eaee082dc243679
Eduard Bissell <edd.bissell@hotmail.co.uk>
2023-10-10 15:51:56 +0100

    first draft of card

--- src/index.js
+++ src/index.js
@@ -1,6 +1,76 @@
+const createCard = (pokemonEntry) => {
+  const liParent = cardGallery;
+  const liAttr = [["class", "card"]];
+  const li = elementAdd("li", liParent, liAttr);
 
-console.log(data);
+  const h2Attr = [["class", "card-title"]];
+  const h2Text = pokemonEntry.name;
+  elementAdd("h2", li, h2Attr, h2Text);
 
-//You can start simple and just render a single 
-//pokemon card from the first element
-console.log(data[0]);
+  const imgAttr = [
+    ["width", "256"],
+    ["class", "card--img"],
+    ["src", pokemonEntry.sprites.other["official-artwork"].front_default],
+  ];
+  elementAdd("img", li, imgAttr, null);
+
+  const ulAttr = [["class", "card--text"]];
+  const ul = elementAdd("ul", li, ulAttr, null);
+
+  stats = getStats(pokemonEntry);
+
+  stats.forEach((stat) => {
+    const key = Object.keys(stat)
+    const text = `${key}: ${stat[key]}`
+    elementAdd("li", ul, null, text);
+  });
+};
+
+const elementAdd = (tagName, tagParent, attributePairs, text) => {
+  const element = document.createElement(tagName);
+
+  addAttr(attributePairs, element);
+
+  element.innerText = text;
+
+  tagParent.append(element);
+
+  return element;
+};
+
+const select = (selector) => {
+  return document.querySelector(selector);
+};
+
+const getStats = (pokemonEntry) => {
+  const stats = pokemonEntry.stats;
+
+  let results = [];
+
+  Object.keys(stats).forEach((idx) => {
+    const entry = stats[idx];
+    const name = entry.stat.name;
+    const base_stat = entry.base_stat;
+
+    results.push({
+      [name]: base_stat,
+    });
+  });
+
+  return results;
+};
+
+function addAttr(attributePairs, element) {
+  if (attributePairs) {
+    attributePairs.forEach((pair) => {
+      [key, value] = pair;
+      element.setAttribute(key, value);
+    });
+  }
+}
+
+const pokemonEntry = data[0];
+
+const cardGallery = select(".cards");
+
+createCard(pokemonEntry);
