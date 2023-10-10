bb4abecc096e7c95b90058b2cb8f76ea82e4cbcb
Jack Miller <jdmiller1991@outlook.com>
2023-10-10 17:56:05 +0100

    Done

--- src/index.js
+++ src/index.js
@@ -1,42 +1,44 @@
-const pokemonCardList = document.querySelectorAll('.cards');
+const pokemonCardList = document.querySelector('.cards');
+
+for (let i = 0; i < data.length; i++) {
+
+    const pokemon = data[i]
 
 
+    // Card Structure
 
+    const pokemonCard = document.createElement('li');
+    pokemonCard.style.listStyleType = 'none'
+    pokemonCard.setAttribute('class', 'card');
+    pokemonCardList.append(pokemonCard);
 
+    // Card Title
 
+    const pokemonCardTitle = document.createElement('h2');
+    pokemonCardTitle.setAttribute('class', 'h2');
+    pokemonCardTitle.innerText = pokemon.name
+    pokemonCard.append(pokemonCardTitle);
 
-// Card Structure
+    // Card Image
 
-const pokemonCard = document.createElement('li');
-pokemonCard.style.listStyleType = 'none'
-pokemonCard.setAttribute('class', '.card');
-pokemonCardList.append(pokemonCard);
+    const pokemonCardImage = document.createElement('img');
+    pokemonCardImage.setAttribute('class', 'card--img', 'img')
+    pokemonCardImage.setAttribute('src', pokemon.sprites.front_default);
+    pokemonCard.append(pokemonCardImage)
 
-// Card Title
+    // Card Stats
 
-const pokemonCardTitle = document.createElement('h2');
-pokemonCardTitle.setAttribute('class', 'h2');
-pokemonCardList.append(pokemonCardTitle);
+    const pokemonCardStats = document.createElement('ul');
+    pokemonCard.style.listStyleType = 'none'
+    pokemonCard.setAttribute('class', 'card--text');
+    pokemonCard.append(pokemonCardStats);
+    for (let j = 0; j < pokemon.stats.length; j++) {
+        const statName = pokemon.stats[j].stat.name
+        const statNumber = pokemon.stats[j].base_stat
+        const pokemonStats = document.createElement('li');
+        pokemonStats.innerText = `${statName}: ${statNumber}` .toUpperCase()
+        pokemonCardStats.append(pokemonStats)
+    }
+    
 
-// Card Image
-
-const pokemonCardImage = document.createElement('img');
-pokemonCardImage.setAttribute('class', '.card--img', 'img')
-pokemonCardImage.setAttribute('src', data.sprites.front_default);
-pokemonCardList.append(pokemonCardImage);
-
-// Card Stats
-
-const pokemonCardStats = document.createElement('ul');
-pokemonCard.style.listStyleType = 'none'
-pokemonCard.setAttribute('class', '.card--text');
-pokemonCardList.append(pokemonCardStats);
-
-
-
-
-console.log(data);
-
-
-
-console.log(data[0]);
+}
