8a0dbb7415f6f2ab9fb2ffcfb514b0101cf91537
AKINDELE AYORINDE <AKINDELEAYORINDE55@GMAIL.COM>
2023-10-11 10:54:31 +0200

    exercises done

--- src/index.js
+++ src/index.js
@@ -29,28 +29,28 @@ for (let i = 0; i < data.length; i++) {
   card.append(features);
 
   const hp = document.createElement("li");
-  hp.innerText = "HP: " + data[i].stats[0].base_stat;
+  hp.innerText = `HP: ${data[i].stats[0].base_stat}`;
+
   features.append(hp);
 
   const attack = document.createElement("li");
-  attack.innerText = "ATTACK: " + data[i].stats[1].base_stat;
+  attack.innerText = `ATTACk:  ${data[i].stats[1].base_stat}`;
   features.append(attack);
 
   const defense = document.createElement("li");
-  defense.innerText = "DEFENSE: " + data[i].stats[2].base_stat;
+  defense.innerText = `DEFENSE:  ${data[i].stats[2].base_stat}`;
   features.append(defense);
 
   const specialAttack = document.createElement("li");
-  specialAttack.innerText = "SPECIAL - ATTACK : " + data[i].stats[3].base_stat;
+  specialAttack.innerText = `SPECIAL - ATTACK:  ${data[i].stats[3].base_stat}`;
   features.append(specialAttack);
 
   const specialDefense = document.createElement("li");
-  specialDefense.innerText =
-    "SPECIAL - DEFENSE : " + data[i].stats[4].base_stat;
+  specialDefense.innerText = `SPECIAL - DEFENSE : ${data[i].stats[4].base_stat} `;
   features.append(specialDefense);
 
   const speed = document.createElement("li");
-  speed.innerText = "SPEED : " + data[i].stats[5].base_stat;
+  speed.innerText = `SPEED: ${data[i].stats[5].base_stat} `;
   features.append(speed);
 }
 console.log(data[0]);
