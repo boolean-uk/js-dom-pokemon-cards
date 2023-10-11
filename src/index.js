83fe863c84cd743e863e111e8a0f456a17c4b46d
Chloe Zermatten <chloezermatten@gmail.com>
2023-10-11 10:29:26 +0100

    index.js refactor (core only)

--- src/index.js
+++ src/index.js
@@ -1,67 +1,72 @@
 
 console.log(data);
+let card
+let stats
+
+const createCard = () => {
+    const list = document.querySelector('ul')
+    card = document.createElement('li')
+    card.setAttribute('class','card')
+    list.append(card)
+}
+
+const createPokemonName = (element) => {
+    const pokemonName = document.createElement('h2')
+    pokemonName.innerText = `${element.name}`
+    pokemonName.setAttribute('class', 'card--title')
+    card.append(pokemonName)
+}
+
+function renderImage(element) {
+
+    const image = document.createElement('img')
+    image.setAttribute('class','card--img')
+
+    image.setAttribute('width', '256')
+
+    image.setAttribute('src', `${element.sprites.other["official-artwork"].front_default}`)
+    card.append(image)
+
+}
+
+function renderStats() {
+    stats = document.createElement('ul')
+    stats.setAttribute('class', 'card--text')
+    card.append(stats)
+
+}
+
+function renderIndividualStat(s) {
+        const statsElement = document.createElement('li')
+        stats.append(statsElement)
+        statsElement.innerText = `${s.stat.name}: ${s.base_stat}`   
+}
 
 data.forEach(element => {
 
-//CARD
-const list = document.querySelector('ul')
-const card = document.createElement('li')
-card.setAttribute('class','card')
-list.append(card)
+    createCard()
+    createPokemonName(element)
+    renderImage(element)
+    renderStats(element)
+
+    element.stats.forEach(s => renderIndividualStat(s))
 
 
-//NAME
-const pokemonName = document.createElement('h2')
-    pokemonName.innerText = `${element.name}`
-    pokemonName.setAttribute('class', 'card--title')
-card.append(pokemonName)
-
-//IMAGE
-
-const image = document.createElement('img')
-image.setAttribute('class','card--img')
-
-image.setAttribute('width', '256')
-
-image.setAttribute('src', `${element.sprites.other["official-artwork"].front_default}`)
-card.append(image)
+//not the most informative, just tried to add a section 
+// const games = document.createElement('ul')
+// card.append(games)
 
 
-//STATS
-const stats = document.createElement('ul')
-stats.setAttribute('class', 'card--text')
-card.append(stats)
+// element.game_indices.forEach(g => {
 
-
-
-element.stats.forEach(s => {
-
-    const statsElement = document.createElement('li')
-    stats.append(statsElement)
-    statsElement.innerText = `${s.stat.name}: ${s.base_stat}`
-})
-
-
-//additional section 
-
-const appearances = document.createElement('ul')
-card.append(appearances)
-
-
-element.game_indices.forEach(g => {
-
-    const game = document.createElement('li') 
-    appearances.append(game)
-    game.innerText = `name: ${g.version.name}` 
+// const game = document.createElement('li') 
+// games.append(game)
+// game.innerText = `${g.version.name}` 
     
-})
-
-
-
+// })
 
 })
 
-console.log(data[0]);
 
 //side note: earlier versions had ${data[data.indeOf(element].name} 
 // instead of just, well, ${element.name} 
