3a4562f5c59982541bac73cee579b0bfd382589c
LAVINIABENZAR <loredanabenzar2@gmail.com>
2023-10-18 11:25:22 +0100

    fixed

--- src/index.js
+++ src/index.js
@@ -6,51 +6,57 @@ console.log(data);
 console.log(data[0]);
 
 
+
+
 function pokemonCard(element) {
-    const cardLi = document.createAttribute('li')
-    newCard.setAttribute('class', 'card')
 
-    title(element, cardLi);
-    Img(element, cardLi);
-    listCharacteristics(element, cardLi);
-
-    const cardUl = document.body.querySelector('ul');
+    const cardLi = document.createElement('li')
+    cardLi.setAttribute('class', 'card')
+    title(element, cardLi)
+    img(element, cardLi)
+    listCharacteristics(element, cardLi)
+    const cardUl = document.body.querySelector('ul')
     cardUl.append(cardLi)
+
 }
 
 function title(element, cardLi) {
     const titleHeader = document.createElement('h2')
     let name = data[element].name
-    titleHeader.innerText = name
+    titleHeader.innerText = name;
     titleHeader.setAttribute('class', 'card--title')
     cardLi.append(titleHeader)
+
 }
 
 function img(element, cardLi) {
-    const cardImg = document.createElement('img')
-    let imgSrc = data[element].sprites.other["oficial-artwork"].front_default
-    cardImg.setAttribute('class', 'card--img')
-    cardImg.setAttribute('width', '256')
-    cardImg.setAttribute('src',`$(imgSrc)`)
-    cardLi.append(cardImg)
+    const pokemonImage = document.createElement('img')
+    const imgSrc = data[element].sprites.other["official-artwork"].front_default
+    pokemonImage.setAttribute('class', 'card--img')
+    pokemonImage.setAttribute('width', '256')
+    pokemonImage.setAttribute('src', `${imgSrc}`)
+    cardLi.append(pokemonImage)
 }
 
 function listCharacteristics(element, cardLi) {
     const stats = data[element].stats
-    const cardCharacteristics = document.createElement('ul')
-    cardCharacteristics.setAttribute('class', 'card--text')
+    const pokemonCharateristics = document.createElement('ul')
+    pokemonCharateristics.setAttribute('class', 'card--text')
 
-    for (let i = 0; i < stats.lenght; i++) {
+    for (let i = 0; i < stats.length; i++) {
         const statName = document.createElement('li')
         statName.innerText = `${stats[i].stat.name}: ${stats[i].base_stat}`
-        cardCharacteristics.append(statName)
+        pokemonCharateristics.append(statName)
     }
-    cardLi.append(cardCharacteristics)
+    cardLi.append(pokemonCharateristics)
 }
 
 function render(element) {
-    for (let i = 0; i < data.lenght; i++) {
-        card(i)
+    for (let i = 0; i < data.length; i++) {
+        pokemonCard(i)
     }
 }
+
 render()
+
+
