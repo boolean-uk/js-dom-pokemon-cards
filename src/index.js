ef9e5271a41bbfc0c5b348809c13c25eedf4fa37
spectraldesign <spectraldesign1@gmail.com>
2024-02-19 12:37:08 +0100

    Add all sprites and onclick change

--- src/index.js
+++ src/index.js
@@ -1,16 +1,30 @@
 const cards = document.querySelector('.cards');
-// Add an extra section to each card that contains information about which games each pokemon appeared in.
+const spritePaths = [
+    ['other', 'official-artwork', 'front_default'],
+    ['other', 'dream_world', 'front_female'],
+    ['other', 'dream_world', 'front_default'],
+    ['back_default'],
+    ['back_female'],
+    ['back_shiny'],
+    ['back_shiny_female'],
+    ['front_default'],
+    ['front_female'],
+    ['front_shiny'],
+    ['front_shiny_female']
+]
+//Change image when clicking the <img> tag, cycling through the images in pokemon.sprites.other['official-artwork']
 data.forEach(pokemon => {
-    console.log(pokemon.name)
+    let currentImageIndex = 0;
     const card = document.createElement('li');
     card.classList.add('card');
     card.style.listStyle = 'none';
+    const imageUrl = getNestedProperty(pokemon.sprites, spritePaths[0]);
     card.innerHTML = `
         <h2 class="card--title">${pokemon.name}</h2>
         <img
             width="256"
             class="card--img"
-            src="${pokemon.sprites.other['official-artwork'].front_default}"
+            src="${imageUrl}"
         />
         <ul class="card--text">
             <li>HP: ${pokemon.stats[0].base_stat}</li>
@@ -26,7 +40,26 @@ data.forEach(pokemon => {
         </ul>
     `;
     cards.appendChild(card);
+
+    const imageElement = card.querySelector('.card--img');
+    imageElement.addEventListener('click', () => cycleImage(imageElement));
+
+    function cycleImage(img) {
+        let nextImage;
+        while(!nextImage) {
+            currentImageIndex++;
+            currentImageIndex = (currentImageIndex) % spritePaths.length; // Increment the index and cycle back to 0 if it exceeds the array length
+            nextImage = getNestedProperty(pokemon.sprites, spritePaths[currentImageIndex]);
+        }
+        img.src = nextImage;
+    }
 });
+
+// Iterate through the array of keys to get the nested property
+function getNestedProperty(obj, keys) {
+    return keys.reduce((currentObj, key) => (currentObj && currentObj.hasOwnProperty(key)) ? currentObj[key] : null, obj);
+}
+
 
 function capitalize(string) {
     return string.charAt(0).toUpperCase() + string.slice(1);
