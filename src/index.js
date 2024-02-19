a87997ca0730d8f55271acc0570a5cb301d5e8de
Pia Loana Mullenbach <pia.loana.mullenbach@dk.experis.com>
2024-02-19 11:11:43 +0100

    finished extension

--- src/index.js
+++ src/index.js
@@ -6,6 +6,20 @@ console.log(data[0]);
 //the container where I'll be adding the pokemon card
 const cardsContainer = document.querySelector(".cards");
 
+// First, need to store the different image URLs for each card.
+// using the front_default, front_shiny, and other.official-artwork.front_default URLs for toggling.
+//adding an array of image URLs to each pokemon object in the data array
+data.forEach((pokemon) => {
+  pokemon.imageUrls = [
+    pokemon.sprites.front_default,
+    pokemon.sprites.front_shiny,
+    pokemon.sprites.other["official-artwork"].front_default,
+  ];
+  // The initial image index is 0
+  pokemon.currentImageIndex = 0;
+});
+//need to update the src attribute of the <img>
+
 //looping through each element of the data array, calling the elements 'pokemon'
 data.forEach((pokemon) => {
   //for each pokemon in the data array I am creating a new <li> element, a list item,
@@ -31,7 +45,21 @@ data.forEach((pokemon) => {
   image.width = 256;
   //The src attribute of the image is set to the URL of the Pokemon official artwork
   //image which is accesed throught the 'sprite' object in the data array for each pokemon
-  image.src = pokemon.sprites.other["official-artwork"].front_default;
+  //image.src = pokemon.sprites.other["official-artwork"].front_default; //only one image for each pokemon
+  image.src = pokemon.imageUrls[pokemon.currentImageIndex]; //updated to have multiple URLs for each pokemon
+
+  //added eventlistener to listen for clicks in order to toggle
+  image.addEventListener("click", () => {
+    // Increment the current image index when it is clicked, looping back to 0 if it exceeds the array length
+    // ensures that the index stays within the bounds of the imageUrls array length by using the modulo
+    //operator %. This means that if currentImageIndex reaches the end of the array, it wraps around to the beginning (index 0).
+    pokemon.currentImageIndex =
+      (pokemon.currentImageIndex + 1) % pokemon.imageUrls.length;
+
+    //After updating the currentImageIndex, this line updates the src attribute of the <img> element with the new image URL.
+    //It retrieves the URL from the imageUrls array using the updated currentImageIndex.
+    image.src = pokemon.imageUrls[pokemon.currentImageIndex];
+  });
 
   //crearing an <ul> element (unordered list) to display the Pokemon stats.
   const textList = document.createElement("ul");
@@ -49,11 +77,29 @@ data.forEach((pokemon) => {
     textList.appendChild(listItem);
   });
 
+  const textListGames = document.createElement("ul");
+  //adding the 'card--text' class for styling
+  textListGames.classList.add("card--textGames");
+
+  //looping through the stats array of the current pokemon.
+  pokemon.game_indices.forEach((index) => {
+    //for each stat I am creating a new <li> element.
+    const listItemGames = document.createElement("li");
+    //setting the text content of each list item to display the stat name in
+    //uppercase and its base value (look at data array)
+    listItemGames.textContent = `${index.version.name.toUpperCase()}: ${
+      index.game_index
+    }`;
+    //Then appending each list item to the <ul> element created earlier
+    textListGames.appendChild(listItemGames);
+  });
+
   //appending title, image and list of stats to the <li> element (the card)
   //created earlier
   card.appendChild(title);
   card.appendChild(image);
   card.appendChild(textList);
+  card.appendChild(textListGames);
 
   //appending each card to the cardsContainer, which is the <ul> element containing
   //all cards
