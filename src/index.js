6bd61cc61a845bc7255bd4afb6765421064827df
spectraldesign <spectraldesign1@gmail.com>
2024-02-19 11:25:08 +0100

    Core

--- src/index.js
+++ src/index.js
@@ -1,6 +1,24 @@
-
-console.log(data);
-
-//You can start simple and just render a single 
-//pokemon card from the first element
-console.log(data[0]);
+const cards = document.querySelector('.cards');
+data.forEach(pokemon => {
+    console.log(pokemon.name)
+    const card = document.createElement('li');
+    card.classList.add('card');
+    card.style.listStyle = 'none';
+    card.innerHTML = `
+        <h2 class="card--title">${pokemon.name}</h2>
+        <img
+            width="256"
+            class="card--img"
+            src="${pokemon.sprites.other['official-artwork'].front_default}"
+        />
+        <ul class="card--text">
+            <li>HP: ${pokemon.stats[0].base_stat}</li>
+            <li>ATTACK: ${pokemon.stats[1].base_stat}</li>
+            <li>DEFENSE: ${pokemon.stats[2].base_stat}</li>
+            <li>SPECIAL-ATTACK: ${pokemon.stats[3].base_stat}</li>
+            <li>SPECIAL-DEFENSE: ${pokemon.stats[4].base_stat}</li>
+            <li>SPEED: ${pokemon.stats[5].base_stat}</li>
+        </ul>
+    `;
+    cards.appendChild(card);
+});
