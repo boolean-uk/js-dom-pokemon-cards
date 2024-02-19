17537f7882dec1f643601c26159d77770fc39a1c
spectraldesign <spectraldesign1@gmail.com>
2024-02-19 11:31:29 +0100

    Add Games

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 const cards = document.querySelector('.cards');
+// Add an extra section to each card that contains information about which games each pokemon appeared in.
 data.forEach(pokemon => {
     console.log(pokemon.name)
     const card = document.createElement('li');
@@ -19,6 +20,14 @@ data.forEach(pokemon => {
             <li>SPECIAL-DEFENSE: ${pokemon.stats[4].base_stat}</li>
             <li>SPEED: ${pokemon.stats[5].base_stat}</li>
         </ul>
+        <h3>Appeared in:</h3>
+        <ul>
+            ${pokemon.game_indices.map(game => `<li><a href=${game.version.url}>Pokemon ${capitalize(game.version.name)}</a></li>`).join('')}
+        </ul>
     `;
     cards.appendChild(card);
 });
+
+function capitalize(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+}
