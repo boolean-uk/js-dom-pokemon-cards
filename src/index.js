769105d3307a20c9ef08e53b76eaace572a38386
llllllll-l <vincent.werelius@gmail.com>
2024-02-19 09:57:10 +0100

    Core and extenions

--- src/index.js
+++ src/index.js
@@ -9,12 +9,21 @@ const ulElement = document.querySelector(".cards");
 
 function renderingCards() {
   for (const pokemon of data) {
-    const liElement = document.createElement("li");
-    liElement.className = "card";
+    const liElement = createCardElement(pokemon);
+    ulElement.appendChild(liElement);
+  }
+}
 
-    liElement.innerHTML = `
+// refactored from 19-02 lession
+function createCardElement(pokemon) {
+  const liElement = document.createElement("li");
+  liElement.className = "card";
+
+  liElement.innerHTML = `
     <h2 class="card--title">${pokemon.name}</h2>
-    <img width="256" class="card--img" src="${pokemon.sprites.other["official-artwork"].front_default}" alt="${pokemon.name}" />
+    <img width="256" class="card--img" src="${
+      pokemon.sprites.other["official-artwork"].front_default
+    }" alt="${pokemon.name}" />
     <ul class="card--text">
         <li>HP: ${pokemon.stats[0].base_stat}</li>
         <li>ATTACK: ${pokemon.stats[1].base_stat}</li>
@@ -23,34 +32,63 @@ function renderingCards() {
         <li>SPECIAL-DEFENSE: ${pokemon.stats[4].base_stat}</li>
         <li>SPEED: ${pokemon.stats[5].base_stat}</li>
     </ul>
-    <p class="card--text">Generations</p>
+    <!-- Extensions --->
+    <p class="card--text"> Versions </p>
     <ul class="card--text">
-        <li>${pokemon.sprites.vertions}</li)>
-    </ul>
-  `;
-    ulElement.appendChild(liElement);
-  }
+    ${renderVersion(pokemon)}
+    </ul>`;
+
+  const imgElement = liElement.querySelector(".card--img");
+  imgElement.addEventListener("click", () => {
+    handleImage(pokemon);
+  });
+
+  return liElement;
 }
+
+// extension (refactor from 19-02 lession)
+function renderVersion(pokemon) {
+  return Object.keys(pokemon.sprites.versions)
+    .map((generation) => {
+      const games = pokemon.sprites.versions[generation];
+      const gameName = Object.keys(games);
+      return `<li>${generation}: ${gameName.join(", ")} </li>`;
+    })
+    .join("");
+}
+
+// extension (refactor from 19-02 lession)
+function handleImage(pokemon) {
+  //console.log(pokemon.name);
+  const cardElement = document.querySelector(
+    `.card--img[alt="${pokemon.name}"]`
+  );
+
+  //console.log(cardElement);
+  const currentImgSrc = cardElement.src;
+  //console.log(currentImgSrc);
+
+  //console.log(pokemon.sprites.back_default);
+  let newImgSrc = "";
+  if (
+    currentImgSrc === pokemon.sprites.other["official-artwork"].front_default
+  ) {
+    newImgSrc = pokemon.sprites.back_default;
+  } else {
+    newImgSrc = pokemon.sprites.other["official-artwork"].front_default;
+  }
+
+  cardElement.src = newImgSrc;
+}
+
 function main() {
   console.log("Runninv main()");
   renderingCards();
+
+  console.log("main() done");
 }
+document.addEventListener("click", (event) => {
+  console.log(event);
+});
 
 main();
-
-/*<li class="card">
-  <h2 class="card--title">Bulbasaur</h2>
-  <img
-    width="256"
-    class="card--img"
-    src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png"
-  />
-  <ul class="card--text">
-    <li>HP: 45</li>
-    <li>ATTACK: 49</li>
-    <li>DEFENSE: 49</li>
-    <li>SPECIAL-ATTACK: 65</li>
-    <li>SPECIAL-DEFENSE: 65</li>
-    <li>SPEED: 45</li>
-  </ul>
-</li>*/
