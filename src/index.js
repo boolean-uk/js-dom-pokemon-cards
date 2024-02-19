8427ffe5d133284314f60d7a5c4d1f5443e60644
Stian Gaustad <stian.gaustad@no.experis.com>
2024-02-19 13:45:51 +0100

    Extension - Image now change when clicked!

--- src/index.js
+++ src/index.js
@@ -6,7 +6,32 @@ document.addEventListener('DOMContentLoaded', function() {
     //pokemon card from the first element
     // console.log(data);
     data.forEach((pokemon) => {
-        
+        const imagePaths = [
+            pokemon.sprites.other["official-artwork"].front_default,
+            pokemon.sprites.front_default,
+            pokemon.sprites.front_shiny,
+            pokemon.sprites.versions["generation-i"]["red-blue"].front_default,
+            pokemon.sprites.versions["generation-ii"].crystal.front_default,
+            pokemon.sprites.versions["generation-iii"]["ruby-sapphire"].front_default,
+            pokemon.sprites.versions["generation-iv"]["diamond-pearl"].front_default,
+            pokemon.sprites.versions["generation-v"]["black-white"].front_default,
+            pokemon.sprites.versions["generation-vi"]["omegaruby-alphasapphire"].front_default,
+            pokemon.sprites.versions["generation-vii"]["ultra-sun-ultra-moon"].front_default
+        ]
+
+        const CardImageDescription = [
+            'Official artwork',
+            'Default sprite',
+            'Shiny sprite',
+            'Gen 1: Red/Blue sprite',
+            'Gen 2: Crystal sprite',
+            'Gen 3: Ruby/Sapphire sprite',
+            'Gen 4: Diamond/Pearl sprite',
+            'Gen 5: Black/White sprite',
+            'Gen 6: Omega-Ruby/Alpha-Sapphire sprite',
+            'Gen 7: Ultra-Sun/Ultra-Moon sprite'
+        ]
+
         const listItem = document.createElement('li')
         listItem.classList.add("card")
         
@@ -18,11 +43,23 @@ document.addEventListener('DOMContentLoaded', function() {
         listItem.appendChild(header)
 
         // Generate the card image
+        const imageContainer = document.createElement("div")
+        const imageDescription = document.createElement("p")
         const image = document.createElement('img')
         image.width = "256"
         image.classList.add("card--img")
-        image.src = pokemon.sprites.other["official-artwork"].front_default
-        header.appendChild(image)
+        image.src = imagePaths[0]
+        imageDescription.innerHTML = CardImageDescription[0]
+        image.addEventListener("click", function() {
+            let elementIndex = imagePaths.indexOf(image.src)
+            image.src = imagePaths[(elementIndex + 1) % imagePaths.length]
+            imageDescription.innerHTML = CardImageDescription[(elementIndex + 1) % imagePaths.length]
+        })
+        imageDescription.style.fontSize = 10
+
+        imageContainer.appendChild(image)
+        imageContainer.appendChild(imageDescription)
+        header.appendChild(imageContainer)
         
         // Generate the card list
         const cardStats = document.createElement('ul')
