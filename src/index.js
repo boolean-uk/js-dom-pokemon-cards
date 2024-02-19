7ad1e361e2f41f2f0a7eefc6475eb9c88f1a3395
Klara Andersson <klara.andersson@se.experis.com>
2024-02-19 11:48:51 +0100

    Extension

--- src/index.js
+++ src/index.js
@@ -2,25 +2,51 @@
 //You can start simple and just render a single 
 //pokemon card from the first element
 const cardUl = document.querySelector(".cards");
+let nrClicks = 0
+
+function NextImage(card, image) {
+    nrClicks += 1
+    let keyNamme = ''
+    
+    let counter = 0
+    for (const [key, value] of Object.entries(card.sprites)) {
+        counter += 1
+
+        if (counter === nrClicks) {
+            if (card.sprites[key] === null || card.sprites[key] === undefined || typeof card.sprites[key] === Object) {
+                nrClicks += 1
+                continue
+            }
+            keyNamme = key
+        }
+    }
+    
+    image.setAttribute('src', card.sprites[keyNamme])
+}
 
 function renderCards() {
     for (let i = 0; i < data.length; i++) {
         const card = data[i];
+
+        const button = document.createElement('button')
+        button.innerText = "Next Image"
+        
         const cardLi = document.createElement('li');
         cardLi.setAttribute('class', 'card')
-    
+        
         const h2 = document.createElement('h2')
         h2.setAttribute('class', 'card--title')
         h2.innerText = card.name.charAt(0).toUpperCase() + card.name.slice(1);
-
+        
         const image = document.createElement('img')
         image.setAttribute('width', '256')
         image.setAttribute('class', 'card--img')
         image.setAttribute('src', card.sprites.other["official-artwork"].front_default)
+        button.addEventListener('click', (event) => NextImage(card, image))
 
         const textUl = document.createElement('ul')
-        const statLi = document.createElement('li')
         textUl.setAttribute('class', 'card--text')
+        const statLi = document.createElement('li')
         const statLi2 = document.createElement('li')
         const statLi3 = document.createElement('li')
         const statLi4 = document.createElement('li')
@@ -44,6 +70,7 @@ function renderCards() {
         cardLi.appendChild(h2)
         cardLi.appendChild(image)
         cardLi.appendChild(textUl)
+        cardUl.appendChild(button)
         cardUl.appendChild(cardLi)
     }
 }
