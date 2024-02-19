52261d1f8966dde9998f3a84fce2f25c3f7ded61
Najem Hamo <najem.hamo@gmail.com>
2024-02-19 15:42:03 +0100

    Extensions

--- src/index.js
+++ src/index.js
@@ -1,32 +1,64 @@
-// Define the data
 document.addEventListener("DOMContentLoaded", function () {
-  data.forEach(function (pokemon) {
+  const container = document.querySelector(".cards");
+
+  function toggleImage(img, currentIndex) {
+    const versions = data[currentIndex].sprites.versions;
+    const versionNames = Object.keys(versions);
+    const currentVersionIndex = img.dataset.versionIndex
+      ? parseInt(img.dataset.versionIndex)
+      : 0;
+    const nextVersionIndex = (currentVersionIndex + 1) % versionNames.length;
+    const nextVersionName = versionNames[nextVersionIndex];
+    const nextVersion = versions[nextVersionName];
+    const nextImageUrl = Object.values(nextVersion)[0].front_default;
+    img.src = nextImageUrl;
+    img.dataset.versionIndex = nextVersionIndex;
+
+    const genName = img.parentElement.querySelector(".card--generation");
+    genName.textContent = `GENERATION: ${nextVersionName.toUpperCase()}`;
+  }
+
+  data.forEach(function (pokemon, index) {
     const card = document.createElement("li");
     card.classList.add("card");
 
-    // Define a function to get the base stat value
-    function getBaseStat(statName) {
-      const stat = pokemon.stats.find((stat) => stat.stat.name === statName);
-      return stat ? stat.base_stat : null;
-    }
-
-    // Set the innerHTML of the card
     card.innerHTML = `
-        <h2 class="card--title">${pokemon.name}</h2>
-        <img class="card--img" src="${
-          pokemon.sprites.other["official-artwork"].front_default
-        }" alt="${pokemon.name}" />
-        <ul class="card--text">
-          <li>HP: ${getBaseStat("hp")}</li>
-          <li>Attack: ${getBaseStat("attack")}</li>
-          <li>Defense: ${getBaseStat("defense")}</li>
-          <li>Special Attack: ${getBaseStat("special-attack")}</li>
-          <li>Special Defense: ${getBaseStat("special-defense")}</li>
-          <li>Speed: ${getBaseStat("speed")}</li>
-        </ul>
-      `;
+      <h2 class="card--title">${pokemon.name}</h2>
+      <img id="img-${index}" class="card--img" src="${
+      pokemon.sprites.other["official-artwork"].front_default
+    }" alt="${pokemon.name}" data-version-index="0" width="256" height="256"/>
+      <ul class="card--text">
+        <li>HP: ${
+          pokemon.stats.find((stat) => stat.stat.name === "hp").base_stat
+        }</li>
+        <li>Attack: ${
+          pokemon.stats.find((stat) => stat.stat.name === "attack").base_stat
+        }</li>
+        <li>Defense: ${
+          pokemon.stats.find((stat) => stat.stat.name === "defense").base_stat
+        }</li>
+        <li>Special Attack: ${
+          pokemon.stats.find((stat) => stat.stat.name === "special-attack")
+            .base_stat
+        }</li>
+        <li>Special Defense: ${
+          pokemon.stats.find((stat) => stat.stat.name === "special-defense")
+            .base_stat
+        }</li>
+        <li>Speed: ${
+          pokemon.stats.find((stat) => stat.stat.name === "speed").base_stat
+        }</li>
+        <li class="card--generation">GENERATION: ${Object.keys(
+          pokemon.sprites.versions
+        )[0].toUpperCase()}</li>
+      </ul>
+    `;
 
-    // Append the card to the .cards element
-    document.querySelector(".cards").appendChild(card);
+    const image = card.querySelector(`#img-${index}`);
+    image.addEventListener("click", function () {
+      toggleImage(image, index);
+    });
+
+    container.appendChild(card);
   });
 });
