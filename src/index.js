ef4165ab5824319ef68420003cb86f87fea5331c
Najem Hamo <najem.hamo@gmail.com>
2024-02-19 10:50:15 +0100

    Core

--- src/index.js
+++ src/index.js
@@ -1,6 +1,32 @@
+// Define the data
+document.addEventListener("DOMContentLoaded", function () {
+  data.forEach(function (pokemon) {
+    const card = document.createElement("li");
+    card.classList.add("card");
 
-console.log(data);
+    // Define a function to get the base stat value
+    function getBaseStat(statName) {
+      const stat = pokemon.stats.find((stat) => stat.stat.name === statName);
+      return stat ? stat.base_stat : null;
+    }
 
-//You can start simple and just render a single 
-//pokemon card from the first element
-console.log(data[0]);
+    // Set the innerHTML of the card
+    card.innerHTML = `
+        <h2 class="card--title">${pokemon.name}</h2>
+        <img class="card--img" src="${
+          pokemon.sprites.other["official-artwork"].front_default
+        }" alt="${pokemon.name}" />
+        <ul class="card--text">
+          <li>HP: ${getBaseStat("hp")}</li>
+          <li>Attack: ${getBaseStat("attack")}</li>
+          <li>Defense: ${getBaseStat("defense")}</li>
+          <li>Special Attack: ${getBaseStat("special-attack")}</li>
+          <li>Special Defense: ${getBaseStat("special-defense")}</li>
+          <li>Speed: ${getBaseStat("speed")}</li>
+        </ul>
+      `;
+
+    // Append the card to the .cards element
+    document.querySelector(".cards").appendChild(card);
+  });
+});
