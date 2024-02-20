d019cacdfe3889fe7442852c66adabe18116f201
Tølløv Aresvik <tollov.aresvik@no.experis.com>
2024-02-20 15:00:01 +0100

    Script: Completed Core Requirements

--- src/index.js
+++ src/index.js
@@ -1,6 +1,54 @@
+const CARD_IMAGE_WIDTH = 256
+const DATA = data
 
-console.log(data);
+const cards = []
+const cardsListObject = document.getElementsByClassName('cards')[0]
+let clickedCardIndex = -1
+init()
 
-//You can start simple and just render a single 
-//pokemon card from the first element
-console.log(data[0]);
+function init() {
+    addCardsToDocument()
+}
+
+function addCardsToDocument() {
+    DATA.forEach(element => {
+        const cardsObject = addCardToDocument(element)
+        cards.push({'documentObject': cardsObject, 'data': element, 'index': element.index})
+        cardsListObject.append(cardsObject)
+    })
+}
+
+function addCardToDocument(cardData) {
+    const card = document.createElement('li')
+    card.classList.add('card')
+    card.append(addCardHeaderToDocument(cardData.name))
+    card.append(addCardImgToDocument(cardData.sprites.other["official-artwork"].front_default))
+    card.append(addCardStatsToDocument(cardData.stats))
+    return card
+}
+
+function addCardHeaderToDocument(text) {
+    const cardHeader = document.createElement('h2')
+    cardHeader.classList.add('card--title')
+    cardHeader.innerText = text
+    return cardHeader
+}
+
+function addCardImgToDocument(srcURL) {
+    const cardImg = document.createElement('img')
+    cardImg.classList.add('card--img')
+    cardImg.width = CARD_IMAGE_WIDTH
+    cardImg.src = srcURL
+    return cardImg
+}
+
+function addCardStatsToDocument(statsList) {
+    const cardStats = document.createElement('ul')
+    cardStats.classList.add('card--text')
+    statsList.forEach(element => {
+        const stat = document.createElement('li')
+        stat.innerText = element.stat.name.toUpperCase() + ": " + element.base_stat
+        cardStats.append(stat)
+    })
+    return cardStats
+}
