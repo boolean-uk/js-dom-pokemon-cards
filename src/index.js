0aa83a703c16aa67bcfb9bd9d57afb7a772f3994
skjalg <spectraldesign1@gmail.com>
2024-02-20 11:04:08 +0100

    a e s t h e t i c

--- src/index.js
+++ src/index.js
@@ -13,11 +13,10 @@ const spritePaths = [
     ['front_shiny_female']
 ]
 //Change image when clicking the <img> tag, cycling through the images in pokemon.sprites.other['official-artwork']
+let currentImageIndex = 0; // hacky way to color code cards since the json does not contain type info
 data.forEach(pokemon => {
-    let currentImageIndex = 0;
     const card = document.createElement('li');
     card.classList.add('card');
-    card.style.listStyle = 'none';
     const imageUrl = getNestedProperty(pokemon.sprites, spritePaths[0]);
     card.innerHTML = `
         <h2 class="card--title">${capitalize(pokemon.name)}</h2>
@@ -34,15 +33,65 @@ data.forEach(pokemon => {
             <li>SPECIAL-DEFENSE: ${pokemon.stats[4].base_stat}</li>
             <li>SPEED: ${pokemon.stats[5].base_stat}</li>
         </ul>
-        <h3>Appeared in:</h3>
-        <ul>
-            ${pokemon.game_indices.map(game => `<li><a href=${game.version.url}>Pokemon ${capitalize(game.version.name)}</a></li>`).join('')}
-        </ul>
+        <button class="toggle-appeared-in-button">Show more info</button>
+        <div class="appeared-in">
+            <h3>Appeared in:</h3>
+            <ul">
+                ${pokemon.game_indices.map(game => `<li><a href=${game.version.url}>Pokemon ${capitalize(game.version.name)}</a></li>`).join('')}
+            </ul>
+        </div>
     `;
+    // Color code cards based on type
+    if (currentImageIndex < 3){
+        card.style.backgroundColor = "#a5ce96";
+        card.style.borderColor = "#359e4a";
+    }
+    else if (currentImageIndex < 6){
+        card.style.backgroundColor = "#f08069";
+        card.style.borderColor = "#d15136";
+    }
+    else if (currentImageIndex < 9){
+        card.style.backgroundColor = "#51beef";
+        card.style.borderColor = "#05649a";
+    }
+    else if (currentImageIndex < 15){
+        card.style.backgroundColor = "#bad247";
+        card.style.borderColor = "#fed758";
+    }
+    else{
+        card.style.backgroundColor = "darkgrey";
+        card.style.borderColor = "#fed758";
+    }
+
+    currentImageIndex++;
+    const appearedIn = card.querySelector('.appeared-in');
+    appearedIn.style.display = 'none';
     cards.appendChild(card);
 
+    // Toggle appeared-in list with button click
+    const button = card.querySelector('.toggle-appeared-in-button');
+    button.addEventListener('click', () => {
+        appearedIn.style.display = appearedIn.style.display === 'none' ? 'block' : 'none';
+        button.textContent = appearedIn.style.display === 'none' ? 'Show more info' : 'Hide info';
+    });
+    
+    // Cycle through images with click
     const imageElement = card.querySelector('.card--img');
     imageElement.addEventListener('click', () => cycleImage(imageElement));
+
+    // The card should have some horizontal parralax effect when hovered
+    // It should take into account the position of the mouse relative to the center of the card.
+    // The movement should be signifant but never over 45 degrees.
+    // It should only move in the x axis.
+    card.addEventListener('mousemove', (e) => {
+        const cardBoundingRect = card.getBoundingClientRect();
+        const xAxis = (cardBoundingRect.width / 2 - (e.clientX - cardBoundingRect.left)) / 20;
+        const yAxis = (cardBoundingRect.height / 2 - (e.clientY - cardBoundingRect.top)) / 20;
+        card.style.transform = `rotateX(${yAxis}deg) rotateY(${xAxis}deg)`;
+    });
+    card.addEventListener('mouseleave', () => {
+        card.style.transform = 'rotateX(0deg) rotateY(0deg)';
+    });
 
     function cycleImage(img) {
         let nextImage;
@@ -57,12 +106,8 @@ data.forEach(pokemon => {
 
 // Iterate through the array of keys to get the nested property
 function getNestedProperty(obj, keys) {
-    keys.forEach(key => {
-        obj = obj[key];
-    });
-    return obj;
+    return keys.reduce((o, k) => o[k], obj);
 }
-
 
 function capitalize(string) {
     return string.charAt(0).toUpperCase() + string.slice(1);
