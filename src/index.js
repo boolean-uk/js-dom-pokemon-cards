2e69d7a0eca895eb005c198615ce8b2fc6c86213
Benjamin Samuelsen Austestad <ben_7aus@hotmail.com>
2024-02-20 09:41:01 +0100

    Implement pokemon cards

--- src/index.js
+++ src/index.js
@@ -1,6 +1,43 @@
+const cards = document.createElement("ul");
+cards.className = "cards";
+for (let i = 0; i < data.length; i++) {
+  const card = renderCard(data[i]);
+  cards.appendChild(card);
+}
+document.body.appendChild(cards);
 
-console.log(data);
+function renderCard(data) {
+  const container = document.createElement("li");
+  container.classList.add("card");
 
-//You can start simple and just render a single 
-//pokemon card from the first element
-console.log(data[0]);
+  const title = document.createElement("h2");
+  title.innerText = capitalizeFirstLetter(data["name"]);
+  title.setAttribute("class", "card--title");
+
+  const img = document.createElement("img");
+  img.width = 256;
+  img.className = "card--img";
+  img.src = data["sprites"]["front_default"];
+  img.alt = "pokemon-image";
+
+  const stats = document.createElement("ul");
+  stats.className = "card--text";
+  const statsData = data["stats"];
+  for (let i = 0; i < statsData.length; i++) {
+    const stat = statsData[i];
+    const name = stat["stat"]["name"];
+    const baseStat = stat["base_stat"];
+    const listStat = document.createElement("li");
+    listStat.innerText = `${name.toUpperCase()}: ${baseStat}`;
+    stats.appendChild(listStat);
+  }
+
+  container.appendChild(title);
+  container.appendChild(img);
+  container.appendChild(stats);
+  return container;
+}
+
+function capitalizeFirstLetter(word) {
+  return word[0].toUpperCase() + word.slice(1);
+}
