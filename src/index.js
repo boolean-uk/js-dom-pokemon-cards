400261613f362cbec9da7bc47974b2c0f78669b8
Matthias Greeven <matsgreeven@gmail.com>
2024-02-20 10:29:46 +0100

    Finished Core criteria

--- src/index.js
+++ src/index.js
@@ -1,6 +1,36 @@
-
-console.log(data);
-
 //You can start simple and just render a single 
 //pokemon card from the first element
-console.log(data[0]);
+const cardTemplate = (pokemon)=>{
+    // Create an object to store the base stats by stat name
+    const baseStatsByName = {};
+    
+    // Populate the baseStatsByName object
+    pokemon.stats.forEach((stat) => {
+        const statName = stat.stat.name;
+        baseStatsByName[statName] = stat.base_stat;
+    });
+    return `
+<li class="card">
+    <h2 class="card--title">${pokemon.name}</h2>
+    <img
+      width="256"
+      class="card--img"
+      src=${pokemon.sprites.other["official-artwork"].front_default}
+    />
+    <ul class="card--text">
+        <li>HP: ${baseStatsByName["hp"]}</li>
+        <li>ATTACK: ${baseStatsByName["attack"]}</li>
+        <li>DEFENSE: ${baseStatsByName["defense"]}</li>
+        <li>SPECIAL-ATTACK: ${baseStatsByName["special-attack"]}</li>
+        <li>SPECIAL-DEFENSE: ${baseStatsByName["special-defense"]}</li>
+        <li>SPEED: ${baseStatsByName["speed"]}</li>
+    </ul>
+  </li>`
+}
+//Map every pokemon into the cardTemplate
+const CardArray = (pokemonList) => {
+    return pokemonList.map(p=>cardTemplate(p))
+}
+console.log(CardArray(data))
+//Call CardElement from Index.html, fill it with card data
+document.getElementsByClassName('cards')[0].innerHTML = CardArray(data).join('');
