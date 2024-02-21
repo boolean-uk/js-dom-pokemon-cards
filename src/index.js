455a416c1a80ab12dc9b1b2fbbaf9b5b7b40a4d0
Giar Rehani <giarreh1@gmail.com>
2024-02-21 11:58:22 +0100

    Finished core

--- src/index.js
+++ src/index.js
@@ -4,3 +4,37 @@ console.log(data);
 //You can start simple and just render a single 
 //pokemon card from the first element
 console.log(data[0]);
+
+
+window.onload = function() {
+    const cardsContainer = document.querySelector('.cards');
+    data.forEach(pokemon => {
+        // Create the card container
+        const card = document.createElement('div');
+        card.className = 'card';
+
+        // Create the title element
+        const title = document.createElement('h2');
+        title.textContent = pokemon.name;
+        title.className = 'card--title';
+
+        // Create the image element
+        const image = document.createElement('img');
+        image.src = pokemon.sprites.other['official-artwork'].front_default;
+        image.alt = pokemon.name;
+        image.className = 'card--img';
+        // Append all elements to the card container
+        card.appendChild(title);
+        card.appendChild(image);
+        // Create the stats paragraphs
+        for(let i = 0; i < pokemon.stats.length; i++){
+            const stats = document.createElement('p');
+            stats.textContent = `${pokemon.stats[i].stat.name}: ${pokemon.stats[i].base_stat}`;
+            stats.className = 'card--text';
+            card.appendChild(stats);
+        }
+        document.body.appendChild(card);
+        cardsContainer.appendChild(card);
+
+    });
+}
