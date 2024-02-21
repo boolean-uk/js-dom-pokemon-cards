1cfb9095ab871bb7fa6c87838bdd29486cb9fd5a
Gudbrand <gudbrandwd@live.no>
2024-02-21 09:11:13 +0100

    done

--- src/index.js
+++ src/index.js
@@ -17,26 +17,26 @@ const createStats = (stats) => {
 };
 
 const createGameAppearance = (game) => {
-    return `<li>${game.version.name}</li>`
-}
+  return `<li>${game.version.name}</li>`;
+};
 
 const createGameAppearances = (games) => {
-    console.log(games)
-    let gameList = ``
-    for (let i = 0; i < games.length; i++) {
-        gameList += createGameAppearance(games[i]);
-    }
-    return gameList
-}
+  let gameList = ``;
+  for (let i = 0; i < games.length; i++) {
+    gameList += createGameAppearance(games[i]);
+  }
+  return gameList;
+};
 
 const createPokemon = (pokemon) => {
-  const toReturn = `<li class="card">
+  const toReturn = `<li class="card" pokemon-id="${pokemon.name}">
             <h2 class="card--title">${pokemon.name}</h2>
             <img
                 width="256"
                 class="card--img"
                 src="${pokemon.sprites.front_default}"
             />
+            <button class="swap-button">Swap image</button>
             <ul class="card--text">
                 ${createStats(pokemon.stats)}
             </ul>
@@ -45,6 +45,7 @@ const createPokemon = (pokemon) => {
                 ${createGameAppearances(pokemon.game_indices)}
             </ul
         </li>`;
+
   return toReturn;
 };
 const createAllPokemon = (pokemonList) => {
@@ -53,3 +54,24 @@ const createAllPokemon = (pokemonList) => {
 
 document.getElementsByClassName("cards")[0].innerHTML =
   createAllPokemon(data).join("");
+
+const cards = document.querySelectorAll(".card");
+
+cards.forEach((card) => {
+  const button = card.querySelector(".swap-button");
+  button.addEventListener("click", () => {
+    const pokemonId = card.getAttribute("pokemon-id");
+    console.log(pokemonId)
+    let pokemonData
+    data.forEach((pokemon) => {
+        if (pokemon.name === pokemonId) pokemonData = pokemon
+    })
+    console.log(pokemonData)
+    const image = card.querySelector(".card--img");
+    if (image) {
+      console.log("click");
+      //image.src = "https://i.kym-cdn.com/entries/icons/original/000/047/683/cheeseburger_wah.jpg";
+      image.src = pokemonData.sprites.front_shiny;
+    }
+  });
+});
