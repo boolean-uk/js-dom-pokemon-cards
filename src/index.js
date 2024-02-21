792658e725ed6460c059a9fe6bd4e4504cf7ad16
Sebank <sebank@gmail.com>
2024-02-21 10:15:48 +0100

    did all tasks

--- src/index.js
+++ src/index.js
@@ -1,6 +1,61 @@
+let i = 0;
 
-console.log(data);
+const createPokemon = (pokemon) => {
+  let text = `<li class="card">
+  <h2 class="card--title">${pokemon.name}</h2>
+  <img
+    width="256"
+    class="card--img"
+    src=${pokemon.sprites.other['official-artwork'].front_default}
+    id = ${pokemon.name}
+    onclick = "changeImage(document.getElementById('${pokemon.name}'))"
+  />
+  <ul class="card--text">`;
 
-//You can start simple and just render a single 
-//pokemon card from the first element
-console.log(data[0]);
+  pokemon.stats.forEach(stat => {
+    text += `<li>${stat.stat.name.toUpperCase()}: ${stat.base_stat}</li>`
+  });
+
+  text += `<li>Appeared in:`
+  pokemon.game_indices.forEach(game => {
+    text += ` ${game.version.name},`;
+  });
+  text = text.slice(0, -1);
+  text += `.</li>`
+
+  text += `  </ul>
+  </li>`;
+  return text;
+}
+const createAllPokemons = (pokemonList) => {
+  return  pokemonList.map(p => createPokemon(p));
+}
+
+document.getElementsByClassName('cards')[0].innerHTML = createAllPokemons(data).join('');
+
+//[front_default, other.dream_world.front_default]
+
+const flattenJSON = (obj = {}, res = {}, extraKey = '') => {
+  for(key in obj){
+     if(typeof obj[key] !== 'object'){
+        res[extraKey + key] = obj[key];
+     }else{
+        flattenJSON(obj[key], res, `${extraKey}${key}.`);
+     };
+  };
+  return res;
+};
+
+const changeImage = (img) => {
+  console.log(img.src);
+  console.log(img.id);
+  let pokemon = data.find(elm => elm.name == img.id);
+  let images = flattenJSON(pokemon.sprites);
+  let front_default = [];
+  Object.keys(images).forEach(key =>{
+    if ( key.includes("front_default")) front_default.push(images[key]);
+  });
+  console.log(front_default);
+  let index = front_default.findIndex(elm => elm == img.src) + 1;
+  img.src = index == front_default.length ? front_default[0] : front_default[index]
+}
