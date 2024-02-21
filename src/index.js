dc2f7445611112cb314016ab42967e49851e7c8d
pkekkonen <paulina.kekkonen@hotmail.com>
2024-02-21 13:44:51 +0100

    Completed core

--- src/index.js
+++ src/index.js
@@ -1,6 +1,51 @@
+const cardsUl = document.querySelector(".cards")
 
-console.log(data);
+for (const currentData of data) {
+    const cardLi = document.createElement('li')
+    cardLi.classList.add('card')
 
-//You can start simple and just render a single 
-//pokemon card from the first element
-console.log(data[0]);
+    //H2
+    const header = renderHeader(currentData);
+    cardLi.appendChild(header);
+
+    //IMAGE
+    const imgElement = renderImage(currentData);
+    cardLi.appendChild(imgElement);
+
+    //STATS
+    const statsUl = renderStats(currentData);
+    cardLi.appendChild(statsUl)
+
+    // Append the li element to the ul element
+    cardLi.style.listStyleType = 'none'
+    cardsUl.appendChild(cardLi);
+}
+
+function renderHeader(cardData) {
+    const header = document.createElement('h2');
+    header.classList.add('card--title')
+    header.textContent = cardData.name[0].toUpperCase() + cardData.name.slice(1);
+    return header;
+}
+
+function renderImage(cardData) {
+    const imgElement = document.createElement('img');
+    imgElement.src = cardData.sprites.other['official-artwork'].front_default;
+    imgElement.classList.add('card--img')
+    imgElement.width = 256
+    return imgElement;
+}
+
+function renderStats(cardData) {
+    const statsUl = document.createElement('ul');
+    statsUl.classList.add('card--text')
+
+    for (const stat of cardData.stats) {
+        const statLi = document.createElement('li');
+        statLi.textContent = `${stat.stat.name.toUpperCase()}: ${stat.base_stat}`;
+        statLi.style.listStyleType = 'none'
+        statsUl.appendChild(statLi);
+    }
+    return statsUl;
+}
+
