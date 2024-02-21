de596d5bd01f5ca0b04a0302aa3b5b1a252afdad
Maha Riazi <maha.riazi@hotmail.no>
2024-02-21 13:24:02 +0100

    core complete i think

--- src/index.js
+++ src/index.js
@@ -1,6 +1,44 @@
+const pokemonListUL = document.querySelector("#card-list");
 
-console.log(data);
+function renderPokemons() {
+    pokemonListUL.innerHTML = ""
+    pokemonListUL.classList.add("cards");
 
-//You can start simple and just render a single 
-//pokemon card from the first element
-console.log(data[0]);
+    for(let i = 0; i < data.length; i++) {
+        const pokemon = data[i]
+        const pokemonLi = document.createElement('li')
+        pokemonLi.setAttribute('id', pokemon.id)
+        pokemonLi.classList.add("card");
+        
+        const h2 = document.createElement('h2')
+        h2.classList.add('card--title')
+        h2.textContent = pokemon.name.charAt(0).toUpperCase() + pokemon.name.slice(1);
+        
+        const img = document.createElement('img')
+        img.classList.add('card--img')
+        img.setAttribute('src', pokemon.sprites.front_default)
+        img.setAttribute('width', '256')
+        
+        const ul = document.createElement('ul')
+        ul.classList.add('card--text')
+        
+        const stats = pokemon.stats;
+        for (const stat of stats) {
+            const liStat = document.createElement('li')
+            liStat.textContent = `${stat.stat.name.toUpperCase()}: ${stat.base_stat}`
+            ul.appendChild(liStat)
+        }
+        
+        pokemonLi.appendChild(h2)
+        pokemonLi.appendChild(img)
+        pokemonLi.appendChild(ul)
+        
+        pokemonListUL.appendChild(pokemonLi)
+    }
+}
+
+function main() {
+    renderPokemons();
+}
+
+main();
