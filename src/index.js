5fddf753616d3f10375e697e5f375ff3fb75fcc4
Alexander <alexander.ellnestam@gmail.com>
2024-02-21 12:25:20 +0100

    Done with toggle

--- src/index.js
+++ src/index.js
@@ -1,133 +1,118 @@
-console.log(data);
+const cardTemplate = (pokemon)=>{
+  const gameIndices = pokemon.game_indices;
 
-//You can start simple and just render a single
-//pokemon card from the first element
-console.log(data[0]);
+  
+  const getPokemonName = () => {
+      const nameList = gameIndices.map((index) => `<li>${capitalizeBeginingOfName(index.version.name)}</li>`)
+      return `<ul>${nameList.join('')}</ul>`
+  };
 
-const viewOfCards = (pokemon) => {
-  const game = pokemon.game;
-
-  const generateNames = () => {
-    const versionList = game.map(
-      (index) => `<li>${capitalizeFirstLetter(index.version.name)}</li>`
-    )
-    return `<ul>${versionList.join("")}</ul>`
+  
+  const capitalizeBeginingOfName = (str) => {
+      return str.charAt(0).toUpperCase() + str.slice(1)
   }
-
-  const capitalizeFirstLetter = (str) => {
-    return str.charAt(0).toUpperCase() + str.splice(1)
-  }
-
-  const stats = {}
-
-  pokemon.stats.array.forEach((s) => {
-    const statName = stat.stat.name
-    stats[statName] = state.base_stat
+  
+  const pokemonStats = {}
+  
+  
+  pokemon.stats.forEach((stat) => {
+      const statName = stat.stat.name
+      pokemonStats[statName] = stat.base_stat
   })
-
-  const art = getArtworks(pokemon)
+  
+  const artworks = fetchArtwork(pokemon)
 
   return `
-<li class="card" data-pokemon-name="${pokemon.name}" data-current-artwork="0">
-    <h2 class="card-title">${capitalizeFirstLetter(pokemon.name)}</h2>
-    <img
-        width="256"
-        class="card--img"
-        src=${art[0]}
-        />
-        <ul class="card--text">
-        <li>HP: ${stats["hp"]}</li>
-        <li>ATTACK: ${stats["attack"]}</li>
-        <li>DEFENSE: ${stats["defense"]}</li>
-        <li>SPECIAL-ATTACK: ${stats["special-attack"]}</li>
-        <li>SPECIAL-DEFENSE: ${stats["special-defense"]}</li>
-        <li>SPEED: ${stats["speed"]}</li>
-    </ul>
-    <div class="card-appearances">
-    Appearace: ${generateNames()}
-    </div>
+  <li class="card" data-pokemon-name="${pokemon.name}" data-current-artwork="0">
+      <h2 class="card--title">${capitalizeBeginingOfName(pokemon.name)}</h2>
+      <img
+      width="256"
+      class="card--img"
+      src=${artworks[0]}
+      />
+      <ul class="card--text">
+          <li>HP: ${pokemonStats["hp"]}</li>
+          <li>ATTACK: ${pokemonStats["attack"]}</li>
+          <li>DEFENSE: ${pokemonStats["defense"]}</li>
+          <li>SPECIAL-ATTACK: ${pokemonStats["special-attack"]}</li>
+          <li>SPECIAL-DEFENSE: ${pokemonStats["special-defense"]}</li>
+          <li>SPEED: ${pokemonStats["speed"]}</li>
+      </ul>
+      
 
-    <button class="toggle-image-btn">Toggle Image</button>
-
-</li>`
+  </li>`
 }
-
-const getArtworks = (pokemon) => {
-  const urls = []
+const fetchArtwork = (pokemon) => {
+  const urlForArtwork = []
 
   if (pokemon.sprites) {
-    if (pokemon.sprites.front_default) {
-      urls.push(pokemon.sprites.front_default)
-    }
-    if (pokemon.sprites.back_default) {
-      urls.push(pokemon.sprites.back_default)
-    }
-
-    if (pokemon.sprites.front_shiny) {
-      urls.push(pokemon.sprites.front_shiny)
-    }
-    if (pokemon.sprites.back_shiny) {
-      urls.push(pokemon.sprites.back_shiny)
-    }
-
-    if (pokemon.sprites.other) {
-      if (
-        pokemon.sprites.other["official-artwork"] &&
-        pokemon.sprites.other["official-artwork"].front_default
-      ) {
-        urls.push(pokemon.sprites.other["official-artwork"].front_default)
+      
+      if (pokemon.sprites.front_default) {
+          urlForArtwork.push(pokemon.sprites.front_default)
       }
-      if (
-        pokemon.sprites.other.dream_world &&
-        pokemon.sprites.other.dream_world.front_default
-      ) {
-        urls.push(pokemon.sprites.other.dream_world.front_default)
+      if (pokemon.sprites.back_default) {
+          urlForArtwork.push(pokemon.sprites.back_default)
       }
-    }
 
-    if (pokemon.sprites.versions) {
-      for (const generation in pokemon.sprites.versions) {
-        const versionObj = pokemon.sprites.versions[generation]
-        for (const versionName in versionObj) {
-          const versionSprites = versionObj[versionName]
-          if (versionSprites.front_default) {
-            urls.push(versionSprites.front_default)
+     
+      if (pokemon.sprites.front_shiny) {
+          urlForArtwork.push(pokemon.sprites.front_shiny)
+      }
+      if (pokemon.sprites.back_shiny) {
+          urlForArtwork.push(pokemon.sprites.back_shiny)
+      }
+
+      
+      if (pokemon.sprites.other) {
+          if (pokemon.sprites.other["official-artwork"] && pokemon.sprites.other["official-artwork"].front_default) {
+              urlForArtwork.push(pokemon.sprites.other["official-artwork"].front_default)
           }
-          if (versionSprites.back_default) {
-            urls.push(versionSprites.back_default)
+          if (pokemon.sprites.other.dream_world && pokemon.sprites.other.dream_world.front_default) {
+              urlForArtwork.push(pokemon.sprites.other.dream_world.front_default)
           }
-        }
       }
-    }
+
+      
+      if (pokemon.sprites.versions) {
+          for (const generation in pokemon.sprites.versions) {
+              const versionObj = pokemon.sprites.versions[generation]
+              for (const versionName in versionObj) {
+                  const versionSprites = versionObj[versionName]
+                  if (versionSprites.front_default) {
+                      urlForArtwork.push(versionSprites.front_default)
+                  }
+                  if (versionSprites.back_default) {
+                      urlForArtwork.push(versionSprites.back_default)
+                  }
+                 
+              }
+          }
+      }
   }
 
-  return urls.filter(Boolean);
+  return urlForArtwork.filter(Boolean)
 }
 
-const CardArray = (pokemonList) => {
-  return pokemonList.map((p) => viewOfCards(p))
+
+const Cards = (pokemonList) => {
+  return pokemonList.map(p=>cardTemplate(p))
 }
 
-document.getElementsByName("cards")[0].innerHTML = CardArray(data).join("");
+document.getElementsByClassName('cards')[0].innerHTML = Cards(data).join('')
 
-const pokemonElements = document.querySelectorAll(".card");
-pokemonElements.forEach((card) => {
-  const imageElement = pokemonElements.querySelector(".card--img")
 
-  imageElement.addEventListener("click", () => {
-    const nameOfPokemon = pokemonElements.getAttribute("data-pokemon-name")
-    const pokemon = data.find((p) => p.name === pokemonName)
-    const artworkIndex = parseInt(
-      pokemonElements.getAttribute("data-current-artwork"),
-      10
-    )
-    const artworks = getArtworks(pokemon);
-    const nextArtworkIndex = (artworkIndex + 1) % artworks.length
+const cardAttributes = document.querySelectorAll('.card')
+cardAttributes.forEach(cardElement => {
+  const imageElement = cardElement.querySelector('.card--img')
 
-    imageElement.setAttribute("src", artworks[nextArtworkIndex])
-    pokemonElements.setAttribute(
-      "data-current-artwork",
-      nextArtworkIndex.toString()
-    )
-  })
-})
+  imageElement.addEventListener('click', () => {
+      const pokemonName = cardElement.getAttribute('data-pokemon-name')
+      const pokemon = data.find(p => p.name === pokemonName);
+      const usingArtworkIndex = parseInt(cardElement.getAttribute('data-current-artwork'), 10)
+      const artworks = fetchArtwork(pokemon)
+      const nextArtworkIndex = (usingArtworkIndex + 1) % artworks.length
+
+      imageElement.setAttribute('src', artworks[nextArtworkIndex])
+      cardElement.setAttribute('data-current-artwork', nextArtworkIndex.toString())
+  });
+});
