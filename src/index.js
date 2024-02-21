5e5764955a1971566b353f799a9bba41cf5adb77
Nora Hønnåshagen Hansen <nora.hans1@hotmail.com>
2024-02-21 13:01:47 +0100

    Core

--- src/index.js
+++ src/index.js
@@ -1,6 +1,52 @@
+const cardList = document.querySelector(".cards")
+
 
 console.log(data);
 
 //You can start simple and just render a single 
 //pokemon card from the first element
 console.log(data[0]);
+
+function loadCard(pokemon) {
+    const cardLi = document.createElement("li")
+    cardLi.setAttribute('class', 'card')
+
+    let cardTitle = document.createElement("h2")
+    cardTitle.setAttribute('class', 'card--title')
+    cardTitle.innerHTML = pokemon.name
+    cardLi.appendChild(cardTitle)
+
+    let cardImg = document.createElement("img")
+    cardImg.setAttribute('width', '256')
+    cardImg.setAttribute('class', 'card--img')
+    cardImg.setAttribute('src', pokemon.sprites.other['official-artwork'].front_default)
+    console.log(pokemon.sprites.other['official-artwork'].front_default)
+    cardLi.appendChild(cardImg)
+
+    let cardUL = document.createElement("ul")
+    for (const stat in pokemon.stats) {
+        let cardLi = document.createElement("li")
+        const str = pokemon.stats[stat].stat.name.toUpperCase() + ': ' + pokemon.stats[stat].base_stat
+        console.log(str)
+        cardLi.innerHTML = str
+        cardUL.appendChild(cardLi)
+    }
+    cardLi.appendChild(cardUL)
+
+    return cardLi
+}
+
+// Render the cards
+function renderCards() {
+    const cards = []
+    for (let i = 0; i < data.length; i++) {
+        cardList.appendChild(loadCard(data[i]))
+    }
+}
+
+
+function main() {
+    renderCards()
+}
+
+main()
