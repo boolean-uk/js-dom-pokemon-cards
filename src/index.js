71b6682916d6465c573ba4c9f6b2248dd8e088ff
spectraldesign <spectraldesign1@gmail.com>
2024-02-22 14:11:22 +0100

    Improve aesthetics

--- src/index.js
+++ src/index.js
@@ -22,12 +22,16 @@ data.forEach(pokemon => {
         <div class="card-glow-effect"></div>
         <div class="card-shiny-overlay"></div>
         <div class="card-shiny-texture"></div>
+        <div class="card-base-shiny-texture"></div>
         <h2 class="card--title">${capitalize(pokemon.name)}</h2>
-        <img
-            width="256"
-            class="card--img"
-            src="${imageUrl}"
-        />
+        <div class="image--container">
+            <div class="card--img--background"> </div>
+            <img
+                width="256"
+                class="card--img"
+                src="${imageUrl}"
+            />
+        </div>
         <ul class="card--text">
             <li>HP: ${pokemon.stats[0].base_stat}</li>
             <li>ATTACK: ${pokemon.stats[1].base_stat}</li>
@@ -48,38 +52,28 @@ data.forEach(pokemon => {
     const overlay = card.querySelector('.card-shiny-overlay');
     if (currentImageIndex < 3) {
         card.style.backgroundColor = "#a5ce96";
-        card.style.borderColor = "#359e4a";
-        if (Math.random() > 0) {
-            overlay.style.backgroundImage = "linear-gradient(-45deg, #ff5781, #5bbe9d)"
-        }
+        //card.style.borderColor = "#359e4a";
+        overlay.style.backgroundImage = "linear-gradient(-45deg, #ff5781, #5bbe9d)"
     }
     else if (currentImageIndex < 6) {
         card.style.backgroundColor = "#f08069";
         card.style.borderColor = "#d15136";
-        if (Math.random() > 0.8) {
-            overlay.style.backgroundImage = "linear-gradient(-45deg, #ff5781, #5bbe9d)"
-        }
+        overlay.style.backgroundImage = "linear-gradient(-45deg, #ff5781, #5bbe9d)"
     }
     else if (currentImageIndex < 9) {
         card.style.backgroundColor = "#51beef";
         card.style.borderColor = "#05649a";
-        if (Math.random() > 0.8) {
-            overlay.style.backgroundImage = "linear-gradient(-45deg, #ff5781, #5bbe9d)"
-        }
+        overlay.style.backgroundImage = "linear-gradient(-45deg, #ff5781, #5bbe9d)"
     }
     else if (currentImageIndex < 15) {
         card.style.backgroundColor = "#bad247";
         card.style.borderColor = "#fed758";
-        if (Math.random() > 0.8) {
-            overlay.style.backgroundImage = "linear-gradient(-45deg, #FE58D2, #587FFE)"
-        }
+        overlay.style.backgroundImage = "linear-gradient(-45deg, #b381a5, #6b7aac)"
     }
     else {
         card.style.backgroundColor = "darkgrey";
         card.style.borderColor = "#fed758";
-        if (Math.random() > 0.8) {
-            overlay.style.backgroundImage = "linear-gradient(-45deg, #ff5781, #5bbe9d)"
-        }
+        overlay.style.backgroundImage = "linear-gradient(-45deg, #ff5781, #5bbe9d)"
     }
 
     currentImageIndex++;
@@ -98,6 +92,10 @@ data.forEach(pokemon => {
     const imageElement = card.querySelector('.card--img');
     imageElement.addEventListener('click', () => cycleImage(imageElement));
 
+
+    // Shiny texture
+    const cardShinyTexture = card.querySelector('.card-shiny-texture');
+
     // Parralax effect on hover
     card.addEventListener('mousemove', (e) => {
         const cardBoundingRect = card.getBoundingClientRect();
@@ -106,13 +104,39 @@ data.forEach(pokemon => {
         card.style.setProperty('--x', relativeX + 'px');
         card.style.setProperty('--y', relativeY + 'px');
 
-        const xAxis = (cardBoundingRect.width / 2 - relativeX) / 20;
-        const yAxis = (cardBoundingRect.height / 2 - relativeY) / 20;
-        card.style.transform = `rotateX(${yAxis}deg) rotateY(${xAxis}deg)`;
+        const { clientX, clientY } = e;
+        const { left, top, width, height } = card.getBoundingClientRect();
+
+        const centerX = left + width / 2;
+        const centerY = top + height / 2;
+
+        const deltaX = clientX - centerX;
+        const deltaY = centerY - clientY;
+
+        cardShinyTexture.style.setProperty('--mask-x', `${deltaX}px`);
+        cardShinyTexture.style.setProperty('--mask-y', `${-deltaY}px`);
+        cardShinyTexture.style.opacity = 1;
+
+        const rotationX = deltaY / height * 20;
+        const rotationY = deltaX / width * 20;
+
+        card.style.transform = `perspective(1000px) rotateX(${-rotationX}deg) rotateY(${-rotationY}deg)`;
+        imageElement.style.transform = `scale(1.1) perspective(700px) rotateX(${rotationX}deg) rotateY(${rotationY}deg)`;
+
+        const normalizedDeltaX = deltaX / (width / 2);
+        const normalizedDeltaY = deltaY / (height / 2);
+
+        const clampedDeltaX = normalizedDeltaX * 30;
+        const clampedDeltaY = normalizedDeltaY * 30;
+        imageElement.style.filter = `brightness(1.1) contrast(1.1) drop-shadow(${-clampedDeltaX}px ${clampedDeltaY}px 15px rgba(2, 2, 0, 0.3))`;
+
     });
     // Reset transform on mouseleave
     card.addEventListener('mouseleave', () => {
         card.style.transform = 'rotateX(0deg) rotateY(0deg)';
+        imageElement.style.transform = 'scale(1) perspective(500px) rotateX(0deg) rotateY(0deg)'
+        imageElement.style.filter = `brightness(1) contrast(1) drop-shadow(0px 20px 10px rgba(2, 2, 0, 0.2))`
+        cardShinyTexture.style.opacity = 0;
     });
 
     // Cycle through images when clicking the image
