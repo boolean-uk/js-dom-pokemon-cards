5b795e5ac7cb90e3f9d03c56a747d0a0be5e5070
Marit Kristine Moe <marit.moe2@ntebb.no>
2024-02-22 14:13:24 +0100

    Added game section to each card.

--- src/index.js
+++ src/index.js
@@ -11,27 +11,22 @@ function renderCards() {
     // Iterate through cards
     for (let i = 0; i < data.length; i++) {
         console.log(data[i]);
-        
         // Make list item
         const cardLi = document.createElement('li')
         cardLi.setAttribute('class', 'card')
-
         // Get card data
         const cardData = data[i]
-
         // Add title
         cardLi.appendChild(makeCardTitle(cardData))
-
         // Add image
         cardLi.appendChild(makeImageElement(cardData))
-        
         // Add text
         cardLi.appendChild(makeTextList(cardData))
-
+        // Add game section
+        cardLi.appendChild(makeGameSection(cardData))
         // Add card to cardList
         cardList.appendChild(cardLi)
     }
-    
 }
 
 function makeCardTitle(card) {
@@ -76,6 +71,31 @@ function addLiWithText(card, j) {
 
 function capitalizeTitle(title) {
     return title[0].toUpperCase() + title.slice(1)
+}
+
+function makeGameSection(card) {
+    const gamesSection = document.createElement('div')
+    // Make a title for  the games section
+    const sectionTitle = document.createElement('h3')
+    sectionTitle.innerText = 'Games ' + capitalizeTitle(card.name) + ' appeared in:'
+    gamesSection.appendChild(sectionTitle)
+    
+    const games = card.game_indices
+    let gamesString = ''
+    // Iterate through games
+    for (let k = 0; k < games.length; k++) {
+        if (k !== 0) {
+            gamesString += ', '
+        }
+        gamesString += games[k].version.name
+    }
+    // Add the games to a paragraph element
+    const gamesPara = document.createElement('p')
+    gamesPara.setAttribute('class', 'card--text')
+    gamesPara.innerText = gamesString
+    // Add games info to the games section
+    gamesSection.appendChild(gamesPara)
+    return gamesSection
 }
 
 function main() {
