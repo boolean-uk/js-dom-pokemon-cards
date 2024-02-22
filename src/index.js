2a7f74aaed3330b8e7644c5663fd5db574fc6735
Alexander Gatland <alexander.gatland@no.experis.com>
2024-02-22 10:29:41 +0100

    Complete core and extension

--- src/index.js
+++ src/index.js
@@ -1,6 +1,65 @@
 
-console.log(data);
 
-//You can start simple and just render a single 
-//pokemon card from the first element
-console.log(data[0]);
+const cardList = document.querySelector(".cards")
+
+
+data.forEach(pokemon => {
+    const pokemonCard = document.createElement('li')
+    pokemonCard.className = 'card'
+
+    const pokemonName = document.createElement('h2')
+    pokemonName.className = 'card--title'
+    pokemonName.textContent = pokemon.name.charAt(0).toUpperCase() + pokemon.name.slice(1)
+    pokemonCard.appendChild(pokemonName)
+
+    const pokemonImg = document.createElement('img')
+    pokemonImg.width = 256
+    pokemonImg.className = 'card--img'
+    pokemonImg.src = pokemon.sprites.other['official-artwork'].front_default
+    pokemonCard.append(pokemonImg)
+
+    const pokemonInfo = document.createElement('ul')
+    pokemonInfo.className = 'card--text'
+
+    pokemon.stats.forEach((stat) => {
+        const statLi = document.createElement('li')
+        statLi.textContent = `${stat.stat.name.toUpperCase()}: ${stat.base_stat}`
+        pokemonInfo.append(statLi)
+    })
+    pokemonCard.append(pokemonInfo)
+
+    const pokemonGameAppearances = document.createElement('ul')
+    pokemonGameAppearances.className = 'card--text'
+    const gameAppearances = document.createElement('li')
+    
+    let games = '';
+    pokemon.game_indices.forEach((game_indice) => {
+        games += game_indice.version.name + ", "
+    })
+    gameAppearances.textContent = "Games: " + games
+    pokemonGameAppearances.append(gameAppearances)
+
+    pokemonCard.append(pokemonGameAppearances)
+
+    cardList.append(pokemonCard)
+})
+
+const cards = document.querySelectorAll(".card")
+
+var swapImage = function() {
+    pokemonName = this.children[0].textContent;
+    
+    data.forEach((pokemon) => {
+        if (pokemon.name == pokemonName.toLowerCase()) {
+            if (this.children[1].src === pokemon.sprites.other.dream_world.front_default) {
+                this.children[1].src = pokemon.sprites.other['official-artwork'].front_default
+            } else {
+                this.children[1].src = pokemon.sprites.other.dream_world.front_default
+            }
+        }
+    })
+}
+
+for (var i = 0; i < cards.length; i++) {
+    cards[i].addEventListener('click', swapImage, false)
+}
