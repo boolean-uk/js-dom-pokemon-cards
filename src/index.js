309fd8ca88ea612230c20b737087b83a5da704bd
Maha Riazi <maha.riazi@hotmail.no>
2024-02-22 14:32:22 +0100

    improved solution for extension 1

--- src/index.js
+++ src/index.js
@@ -1,18 +1,30 @@
-const pokemonListUL = document.querySelector("#card-list");
+const pokemonListUL = document.querySelector('#card-list')
+const gamesButton = document.querySelector('#games-button')
+
+let showGames = false
+
+function toggleGames(){
+    showGames = !showGames
+    renderPokemons()
+}
+
+function registerButtonClicked(){
+    gamesButton.addEventListener('click', toggleGames)
+}
 
 function renderPokemons() {
     pokemonListUL.innerHTML = ""
-    pokemonListUL.classList.add("cards");
+    pokemonListUL.classList.add("cards")
 
     for(let i = 0; i < data.length; i++) {
         const pokemon = data[i]
         const pokemonLi = document.createElement('li')
         pokemonLi.setAttribute('id', pokemon.id)
-        pokemonLi.classList.add("card");
+        pokemonLi.classList.add("card")
         
         const h2 = document.createElement('h2')
         h2.classList.add('card--title')
-        h2.textContent = pokemon.name.charAt(0).toUpperCase() + pokemon.name.slice(1);
+        h2.textContent = pokemon.name.charAt(0).toUpperCase() + pokemon.name.slice(1)
         
         const img = document.createElement('img')
         img.classList.add('card--img')
@@ -28,30 +40,32 @@ function renderPokemons() {
             liStat.textContent = `${stat.stat.name.toUpperCase()}: ${stat.base_stat}`
             ul.appendChild(liStat)
         }
-
-        const ul2 = document.createElement('ul')
-        ul2.classList.add('card--game')
-        const games = pokemon.game_indices
-        ul2.textContent = 'GAMES:'
         
-        for (const game of games){
-            const liGame = document.createElement('li')
-            liGame.textContent = `Pokemon ${game.version.name.charAt(0).toUpperCase() + game.version.name.slice(1)}`
-            ul2.appendChild(liGame)
-        }
-
         pokemonLi.appendChild(h2)
         pokemonLi.appendChild(img)
         pokemonLi.appendChild(ul)
-        pokemonLi.appendChild(ul2)
+
+        if (showGames) {
+            const ul2 = document.createElement('ul')
+            ul2.classList.add('card--game')
+            const games = pokemon.game_indices
+            ul2.textContent = 'GAMES:'
+            
+            for (const game of games){
+                const liGame = document.createElement('li')
+                liGame.textContent = `Pokemon ${game.version.name.charAt(0).toUpperCase() + game.version.name.slice(1)}`
+                ul2.appendChild(liGame)
+            }
+            pokemonLi.appendChild(ul2)
+        }
         
         pokemonListUL.appendChild(pokemonLi)
     }
 }
 
 function main() {
-    renderPokemons();
+    registerButtonClicked()
+    renderPokemons()
 }
 
-main();
-
+main()
