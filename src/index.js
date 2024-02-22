89e574cca3e4bcf3c3f03f83411800910de88c9c
ThomasKvam <thomas.e.h.kvam@hotmail.no>
2024-02-22 14:40:57 +0100

    Finished extension 1

--- src/index.js
+++ src/index.js
@@ -14,10 +14,15 @@ function renderPokemon(){
         const image = renderImage(data[i])
         //add stats
         const stats = renderStats(data[i])
+        //add game info
+        const games = gameInfo(data[i])
         //put them together
         pokemonLi.appendChild(header)
         pokemonLi.appendChild(image)
         pokemonLi.appendChild(stats)
+        pokemonLi.appendChild(games)
+
+
         //add the element to the pokemonListUL
         pokemonListUL.appendChild(pokemonLi)
     }
@@ -41,7 +46,7 @@ function renderImage(data){
 
 function renderStats(data){
     const stats = document.createElement('li')
-        stats.classList.add('card--text')
+    stats.classList.add('card--text')
         for(let i = 0; i < data.stats.length; i++){
             const statLi = document.createElement('li')
             statLi.textContent = `${data.stats[i].stat.name.toUpperCase()}: ${data.stats[i].base_stat}`
@@ -49,6 +54,20 @@ function renderStats(data){
         }
     return stats
 }
+
+function gameInfo(data){
+    const gameInfo = document.createElement('li')
+    gameInfo.classList.add('card--text')
+    gameInfo.textContent += 'Pokemon appears in: \n'
+    for(let i = 0; i < data.game_indices.length -1; i++){
+        gameInfo.textContent += `${data.game_indices[i].version.name}` + ', '
+    }
+    gameInfo.textContent += `${data.game_indices[data.game_indices.length -1].version.name}` + '.'
+    console.log(gameInfo)
+    return gameInfo
+}
+
+
 
 function main(){
     renderPokemon()
