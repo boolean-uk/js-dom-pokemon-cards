54aa44a8e32cc3fe6b0a43b6527617028454e368
Alexander Gatland <alexander.gatland@no.experis.com>
2024-02-22 10:45:23 +0100

    refactor code a bit

--- src/index.js
+++ src/index.js
@@ -1,50 +1,67 @@
 
+function renderCards() {
+    const cardList = document.querySelector(".cards")
+    data.forEach(pokemon => {
 
-const cardList = document.querySelector(".cards")
+        cardList.append(renderCard(pokemon))
+    })
+}
 
-
-data.forEach(pokemon => {
+function renderCard(pokemon) {
     const pokemonCard = document.createElement('li')
     pokemonCard.className = 'card'
 
+    pokemonCard.appendChild(createTitle(pokemon.name))
+
+    pokemonCard.append(createImg(pokemon.sprites.other['official-artwork'].front_default))
+
+    pokemonCard.append(createStatInfo(pokemon.stats))
+
+    pokemonCard.append(createGameAppearances(pokemon.game_indices))
+
+    return pokemonCard
+}
+
+function createTitle(name) {
     const pokemonName = document.createElement('h2')
     pokemonName.className = 'card--title'
-    pokemonName.textContent = pokemon.name.charAt(0).toUpperCase() + pokemon.name.slice(1)
-    pokemonCard.appendChild(pokemonName)
+    pokemonName.textContent = name.charAt(0).toUpperCase() + name.slice(1)
+    return pokemonName
+}
 
+function createImg(sprite) {
     const pokemonImg = document.createElement('img')
     pokemonImg.width = 256
     pokemonImg.className = 'card--img'
-    pokemonImg.src = pokemon.sprites.other['official-artwork'].front_default
-    pokemonCard.append(pokemonImg)
+    pokemonImg.src = sprite
+    return pokemonImg
+}
 
+function createStatInfo(stats) {
     const pokemonInfo = document.createElement('ul')
     pokemonInfo.className = 'card--text'
 
-    pokemon.stats.forEach((stat) => {
+    stats.forEach((stat) => {
         const statLi = document.createElement('li')
         statLi.textContent = `${stat.stat.name.toUpperCase()}: ${stat.base_stat}`
         pokemonInfo.append(statLi)
     })
-    pokemonCard.append(pokemonInfo)
+    return pokemonInfo
+}
 
+function createGameAppearances(gameIndices) {
     const pokemonGameAppearances = document.createElement('ul')
-    pokemonGameAppearances.className = 'card--text'
-    const gameAppearances = document.createElement('li')
-    
-    let games = '';
-    pokemon.game_indices.forEach((game_indice) => {
-        games += game_indice.version.name + ", "
-    })
-    gameAppearances.textContent = "Games: " + games
-    pokemonGameAppearances.append(gameAppearances)
-
-    pokemonCard.append(pokemonGameAppearances)
-
-    cardList.append(pokemonCard)
-})
-
-const cards = document.querySelectorAll(".card")
+        pokemonGameAppearances.className = 'card--text'
+        const gameAppearances = document.createElement('li')
+        
+        let games = '';
+        gameIndices.forEach((game_indice) => {
+            games += game_indice.version.name + ", "
+        })
+        gameAppearances.textContent = "Games: " + games
+        pokemonGameAppearances.append(gameAppearances)
+    return pokemonGameAppearances
+}
 
 var swapImage = function() {
     pokemonName = this.children[0].textContent;
@@ -60,6 +77,16 @@ var swapImage = function() {
     })
 }
 
-for (var i = 0; i < cards.length; i++) {
-    cards[i].addEventListener('click', swapImage, false)
+function addClickEventToCards() {
+    const cards = document.querySelectorAll(".card")
+    for (var i = 0; i < cards.length; i++) {
+        cards[i].addEventListener('click', swapImage, false)
+    }
 }
+
+function main() {
+    renderCards()
+    addClickEventToCards()
+}
+
+main()
