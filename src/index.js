5907efbb926e40b2c5986180286cfac11ae3527c
pkekkonen <paulina.kekkonen@hotmail.com>
2024-02-23 07:51:19 +0100

    completed second extension

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 const cardsUl = document.querySelector(".cards");
 
+const imageIndices = {}
+
 for (const currentData of data) {
   const cardLi = document.createElement("li");
   cardLi.classList.add("card");
@@ -28,12 +30,40 @@ function renderHeader(cardData) {
 }
 
 function renderImage(cardData) {
+  // Get all front default images
+  let images = []
+  findAllFrontDefaultImages(cardData, images, cardData.sprites.other["official-artwork"].front_default, cardData.name)
+
   const imgElement = document.createElement("img");
   imgElement.src = cardData.sprites.other["official-artwork"].front_default;
   imgElement.classList.add("card--img");
   imgElement.width = 256;
+
+  imgElement.addEventListener("click", function() {
+    if (imageIndices[cardData.name] + 1 >= images.length) {
+      imageIndices[cardData.name] = 0
+    } else {
+      imageIndices[cardData.name] ++
+    }
+  
+    imgElement.src = images[imageIndices[cardData.name]]
+
+  })
   return imgElement;
 }
+
+function findAllFrontDefaultImages(object, images, original, name) {
+  for (const key in object) {
+      if (typeof object[key] === 'object' && object[key] !== null) {
+          findAllFrontDefaultImages(object[key], images, original, name); 
+      } else if (key === 'front_default') {
+          images.push(object[key]);
+          if (object[key] === original) {
+            imageIndices[name] = images.length - 1
+          }
+      }
+  }
+} 
 
 function renderStats(cardData) {
   const statsUl = document.createElement("ul");
