65eee67d1ed986131d6e7dcc922f9566febc7219
Mathias Fredriksen <mathias.fredriksen@no.experis.com>
2024-02-23 10:40:11 +0100

    Pokemon cards colorized

--- src/index.js
+++ src/index.js
@@ -4,11 +4,43 @@ console.log(data);
 //pokemon card from the first element
 console.log(data[0]);
 
+let cardType = []
+
 function capitalLetter(data){
     return `${data.name.at(0).toUpperCase() + data.name.slice(1)}`
 }
 
+function modifyCardColors() {
+    let grassColor = "rgb(158,220,73)"
+    let fireColor = "rgb(213,111,42)"
+    let waterColor = "rgb(135,197,232)"
+    let normalColor = "rgb(232,238,242)"
+    let bugColor = "rgb(150,221,164)"
+
+    for(let i = 0; i < document.getElementsByClassName("card").length; i++){
+        console.log(`Pokemon ${i}, ${cardType[i]}`)
+        let color = normalColor
+
+        if(cardType.at(i) == "grass")
+            color = grassColor;
+        if(cardType.at(i) == "fire")
+            color = fireColor;
+        if(cardType.at(i) == "water")
+            color = waterColor;
+        if(cardType.at(i) == "bug")
+            color = bugColor;
+        if(cardType.at(i) == "normal")
+            color = normalColor;
+
+        document.getElementsByClassName("card")[i].style.backgroundColor = color;
+    }
+}
+
 function createCard(data) {
+    let pokemonType = ""
+    pokemonType = data.stats[6].type
+    cardType.push(pokemonType)
+
     return `<li class="card">
     <h2 class="card--title">${capitalLetter(data)}</h2>
     <img
@@ -28,7 +60,7 @@ function createCard(data) {
 }
 
 function populateTable(pokemonList) {
-    return pokemonList.map(x => createCard(x))
+    return pokemonList.map(x => createCard(x)) 
 }
-
 document.getElementsByClassName('cards')[0].innerHTML = populateTable(data).join('')
+modifyCardColors();
