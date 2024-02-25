b2cf8936890d0cf22feedce296cc3cdd98c96c73
Ateeb <ateeb@live.no>
2024-02-25 19:26:17 +0100

    extension done

--- src/index.js
+++ src/index.js
@@ -9,8 +9,6 @@ function renderTask(){
     for(i =0; i<data.length; i++){
         const task = data[i]
         const listItem = document.createElement('h2');
-
-
         
         const listAttack = document.createElement('p');
         listAttack.setAttribute('class', 'card--text');
@@ -39,15 +37,32 @@ function renderTask(){
         listHP.setAttribute('class', 'card--text');
         listHP.innerHTML = 'HP: ' + task.stats[0].base_stat;
 
-        const newImage = document.createElement('img')
-        newImage.setAttribute('class', 'card--img')
-        newImage.setAttribute('alt', task.name)
-        newImage.src = task.sprites.front_default
+        const newImage = document.createElement('img');
+        newImage.setAttribute('class', 'card--img');
+        newImage.setAttribute('alt', task.name);
+        newImage.setAttribute('id', 'imgId' + i);
+        newImage.src = task.sprites.front_default; 
+        newImage.setAttribute('src-2', task.sprites.back_default);
+        newImage.onclick = (function(index) {
+            return function() { runCommand(index); };
+        })(i);
 
-        listItem.setAttribute('id',task.id);
+        listItem.setAttribute('id', task.id);
         listItem.innerHTML = task.name;
 
-        
+        let teller = 0;
+        const listGames = document.createElement('p');
+        listGames.setAttribute('class', 'card--text');
+        listGames.innerHTML = 'Games: '
+        task.game_indices.forEach(items => {
+            teller++
+            if(teller%3 === 0){
+                listGames.innerHTML += '\n';
+            }
+            listGames.innerHTML += items.version.name + ',';
+        })
+
+
         listCard.appendChild(listItem);
         listCard.appendChild(newImage);
         listCard.appendChild(listHP);
@@ -56,8 +71,27 @@ function renderTask(){
         listCard.appendChild(listSAttack);
         listCard.appendChild(listDefence);
         listCard.appendChild(listSpeed);
-        taskList.appendChild(listCard)
+        listCard.appendChild(listGames);
+        taskList.appendChild(listCard);
+        
+
         console.log(listItem);
+    }
+}
+
+function runCommand(index) {
+    let imgId = document.getElementById('imgId' + index);
+    console.log('Clicked image for index: ', index);
+    console.log(imgId);
+    let originalSrc = imgId.getAttribute('src-3');
+    let alternateSrc = imgId.getAttribute('src-2');
+
+    if (imgId.src === alternateSrc) {
+        imgId.src = originalSrc; 
+    } else {
+        imgId.setAttribute('src-3', imgId.src)
+        imgId.src = alternateSrc;
+
     }
 }
 
