ce67c40ee4612e58b3171a1f90fe26a54ca7a61b
Myrthe Dullaart <myrtheluca@hotmail.com>
2024-03-25 19:29:19 +0100

    pass first part of extension/add event listener

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ console.log(data);
 
 //You can start simple and just render a single 
 //pokemon card from the first element
-console.log(data[0].game_indices[0].version.name);
+console.log(data[0].sprites);
 
 function newCard() {
     for (let i = 0; i < data.length; i++) {
@@ -20,22 +20,46 @@ function newCard() {
         const newImage = document.createElement('img')
         newImage.classList.add('card--img')
         newImage.style.width = '256px'
-        newImage.src = data[i].sprites.other["official-artwork"].front_default
+        newImage.setAttribute('src', data[i].sprites.other["official-artwork"].front_default) 
         newLi.append(newImage)
 
         const newUl = document.createElement('ul')
         newUl.classList.add('card--text')
         newLi.append(newUl)
         
-        for (let j = 0; j < 6; j++) {
+        for (let j = 0; j < data[i].stats.length; j++) {
             const statsLi = document.createElement('li')
             statsLi.innerText = data[i].stats[j].stat.name.toUpperCase() + ': ' + data[i].stats[j].base_stat
             newUl.append(statsLi)
         }
 
+        const h3 = document.createElement('h3')
+        h3.innerText = 'Games'
+        newLi.append(h3)
+
+        const gamesUl = document.createElement('ul')
+        gamesUl.classList.add('games')
+        newLi.append(gamesUl)
+
+        for (let k = 0; k < data[i].game_indices.length; k++) {
+            const gamesLi = document.createElement('li')
+            gamesLi.innerText = data[i].game_indices[k].version.name
+            gamesUl.append(gamesLi)
+        }
+
         cards[0].append(newLi)
+
+        newLi.addEventListener('click', () => {
+            newImage.setAttribute('src', data[i].sprites.back_default)
+            })
     }
     
 }
 
 newCard()
+
+// function togglePicture(card) {
+//     card.addEventListener('click', () => {
+//         alert('something')
+//     })
+// }
