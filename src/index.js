e5612c4a3941202e13b9f8d6f8fe95fd98a3325f
Angus Townsley <angustownsley@gmail.com>
2024-03-25 12:37:45 +0000

    loop added to populate page

--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
 
-console.log(data);
 
 //You can start simple and just render a single 
 //pokemon card from the first element
@@ -14,16 +13,18 @@ function capitlisation(inputString) {
     return outputString
   }
 
+function cardCreate(index){
+
 const card = document.createElement("li")
 card.className = "card"
 
 const pokemonName = document.createElement("h2")
 pokemonName.className = "card--title"
-nameFix = capitlisation(data[0].name)
+nameFix = capitlisation(data[index].name)
 pokemonName.innerHTML = nameFix
 
 const image = document.createElement("img")
-image.src = data[0].sprites.other["official-artwork"].front_default
+image.src = data[index].sprites.other["official-artwork"].front_default
 image.className = "card--img"
 image.width="256"
 
@@ -31,13 +32,21 @@ const cardText = document.createElement("ul")
 cardText.className = "card--text"
 for(let i = 0; i < 6; i++) {
     const stat = document.createElement("li")
-    stat.innerHTML = data[0].stats[i].stat.name.toUpperCase() + ": " + data[0].stats[i].base_stat
+    stat.innerHTML = data[index].stats[i].stat.name.toUpperCase() + ": " + data[index].stats[i].base_stat
     cardText.appendChild(stat)
 }
-cardsContainer = document.querySelector(".cards")
+
 cardsContainer.appendChild(card)
 card.appendChild(pokemonName)
 card.appendChild(image)
 card.appendChild(cardText)
 
-console.log(data[0]);
+}
+cardsContainer = document.querySelector(".cards")
+
+
+
+
+for(let i = 0; i < data.length ; i++){
+    cardCreate(i)
+}
