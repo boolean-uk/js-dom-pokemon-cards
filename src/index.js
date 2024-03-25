7bce22881956222cd23e1737b0acb13c289c45c7
Hamada Abdelaal <hamada_abdelaal@hotmail.com>
2024-03-25 17:32:31 +0100

    using DOM to render dynamic HTML

--- src/index.js
+++ src/index.js
@@ -1,6 +1,44 @@
+for (let i = 0; i < data.length; i++) {
+  const pokemon = data[i];
 
-console.log(data);
+  const listItem = document.createElement("li");
+  listItem.classList.add("card");
 
-//You can start simple and just render a single 
-//pokemon card from the first element
-console.log(data[0]);
+  const h2 = document.createElement("h2");
+  h2.classList.add("card--title");
+  h2.innerText = pokemon.name;
+
+  listItem.append(h2);
+
+  const image = document.createElement("img");
+  image.classList.add("card--img");
+
+  image.setAttribute("width", 256);
+  image.setAttribute(
+    "src",
+    pokemon.sprites.other["official-artwork"].front_default
+  );
+
+  listItem.append(image);
+
+  const nestedListItem = document.createElement("li");
+  const statsArr = pokemon.stats;
+
+  for (let j = 0; j < statsArr.length; j++) {
+    const name = pokemon.stats[j].stat.name.toUpperCase();
+    const value = pokemon.stats[j].base_stat;
+
+    const p = document.createElement("p");
+    p.innerText = `${name}: ${value}`;
+    nestedListItem.append(p);
+  }
+
+  const cardText = document.createElement("ul");
+  cardText.classList.add("card--text");
+  cardText.append(nestedListItem);
+  listItem.append(cardText);
+
+  const ul = document.querySelector(".cards");
+
+  ul.append(listItem);
+}
